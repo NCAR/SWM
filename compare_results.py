92ebf38b4995162de4137cd7aca6e0401e873638
Zephaniah Connell <zconnell@uwyo.edu>
2021-07-14 17:01:29 -0600

    create preliminary L_inf norm calculation script

---
+++ compare_results.py
@@ -0,0 +1,24 @@
+import numpy as np
+import argparse
+
+
+if __name__ == "__main__":
+    # construct the argument parser and parse the arguments
+    ap = argparse.ArgumentParser()
+    ap.add_argument("-r", "--ref",
+        required=True,
+        help="Path to reference .csv file")
+    ap.add_argument("-t", "--test",
+        required=True,
+        help="Path to test .csv file")
+    args = vars(ap.parse_args())
+
+    ref_data = np.genfromtxt(args['ref'], delimiter=',')
+    test_data = np.genfromtxt(args['test'], delimiter=',')
+
+    # Perform L infinity norm on difference between results
+    diff = np.subtract(ref_data,test_data)
+    print(diff)
+    L_inf_norm = np.linalg.norm(diff, ord=np.inf)
+
+    print("L_inf norm: ", L_inf_norm)
