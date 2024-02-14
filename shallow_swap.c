49e75cff36b057cb67908f90acf96a2abdf7867e
cenamiller <cena.miller@gmail.com>
2024-02-14 08:28:52 -0700

    base unaccelerated code with linear addressing, and reference output from 64x64 data on 4000 iteration

--- shallow_swap.c
+++ shallow_swap.c
@@ -25,7 +25,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
-
+#ifdef _OPENACC
+#include <openacc.h>
+#endif
 #define MIN(x,y) ((x)>(y)?(y):(x))
 #define MAX(x,y) ((x)>(y)?(x):(y))
 
@@ -33,8 +35,9 @@
 #define FALSE 0
 #define M 64
 #define N 64
-#define M_LEN M + 1
-#define N_LEN N + 1
+#define M_LEN (M + 1)
+#define N_LEN (N + 1)
+#define SIZE ((M_LEN)*(N_LEN))
 #define ITMAX 4000
 #define L_OUT TRUE
 
@@ -84,10 +87,22 @@ extern void dswap(double **a, double **b);
 int main(int argc, char **argv) {
   
   // solution arrays
-  double u[M_LEN][N_LEN],v[M_LEN][N_LEN],p[M_LEN][N_LEN];
-  double unew[M_LEN][N_LEN],vnew[M_LEN][N_LEN],pnew[M_LEN][N_LEN];
-  double uold[M_LEN][N_LEN],vold[M_LEN][N_LEN],pold[M_LEN][N_LEN];
-  double cu[M_LEN][N_LEN],cv[M_LEN][N_LEN],z[M_LEN][N_LEN],h[M_LEN][N_LEN],psi[M_LEN][N_LEN];
+  double *u,*v,*p,*unew,*vnew,*pnew,*uold,*vold,*pold,*cu,*cv,*z,*h,*psi;
+
+  u = (double *)malloc(sizeof(double)*M_LEN*N_LEN);
+  v = (double *)malloc(sizeof(double)*M_LEN*N_LEN);
+  p = (double *)malloc(sizeof(double)*M_LEN*N_LEN);
+  unew = (double *)malloc(sizeof(double)*M_LEN*N_LEN);
+  vnew = (double *)malloc(sizeof(double)*M_LEN*N_LEN);
+  pnew = (double *)malloc(sizeof(double)*M_LEN*N_LEN);
+  uold = (double *)malloc(sizeof(double)*M_LEN*N_LEN);
+  vold = (double *)malloc(sizeof(double)*M_LEN*N_LEN);
+  pold = (double *)malloc(sizeof(double)*M_LEN*N_LEN);
+  cu = (double *)malloc(sizeof(double)*M_LEN*N_LEN);
+  cv = (double *)malloc(sizeof(double)*M_LEN*N_LEN);
+  z = (double *)malloc(sizeof(double)*M_LEN*N_LEN);
+  h = (double *)malloc(sizeof(double)*M_LEN*N_LEN);
+  psi = (double *)malloc(sizeof(double)*M_LEN*N_LEN);
 
   double dt,tdt,dx,dy,a,alpha,el,pi;
   double tpi,di,dj,pcf;
@@ -97,10 +112,9 @@ int main(int argc, char **argv) {
   int i,j;
  
   // timer variables 
-  double mfs100,mfs200,mfs300,mfs310;
+  double mfs100,mfs200,mfs300;
   double t100,t200,t300;
   double tstart,ctime,tcyc,time,ptime;
-  double t100i,t200i,t300i;
   double c1,c2;
 
   // ** Initialisations ** 
@@ -126,40 +140,51 @@ int main(int argc, char **argv) {
   pcf = pi * pi * a * a / (el * el);
 
   // Initial values of the stream function and p
+
   for (i=0;i<M_LEN;i++) {
     for (j=0;j<N_LEN;j++) {
-      psi[i][j] = a * sin((i + .5) * di) * sin((j + .5) * dj);
-      p[i][j] = pcf * (cos(2. * (i) * di) + cos(2. * (j) * dj)) + 50000.;
+      int idx = i*N_LEN+j;
+      psi[idx] = a * sin((i + .5) * di) * sin((j + .5) * dj);
+      p[idx] = pcf * (cos(2. * (i) * di) + cos(2. * (j) * dj)) + 50000.;
     }
   }
-    
+  
   // Initialize velocities
+
   for (i=0;i<M;i++) {
     for (j=0;j<N;j++) {
-      u[i + 1][j] = -(psi[i + 1][j + 1] - psi[i + 1][j]) / dy;
-      v[i][j + 1] = (psi[i + 1][j + 1] - psi[i][j + 1]) / dx;
+      int idx01 = (i*N_LEN) + j+1; //[i][j+1]
+      int idx10 = ((i+1)*N_LEN) + j; //[i+1][j]
+      int idx11 = ((i+1)*N_LEN) + j+1; //[i+1][j+1]
+      u[idx10] = -(psi[idx11] - psi[idx10]) / dy;
+      v[idx01] = (psi[idx11] - psi[idx01]) / dx;
     }
   }
      
   // Periodic continuation
+
   for (j=0;j<N;j++) {
-    u[0][j] = u[M][j];
-    v[M][j + 1] = v[0][j + 1];
+    u[j] = u[M*N_LEN+j]; 
+    v[M*N_LEN+j+1] = v[j + 1];
   }
+
   for (i=0;i<M;i++) {
-    u[i + 1][N] = u[i + 1][0];
-    v[i][0] = v[i][N];
+    u[(i + 1)*N_LEN +N] = u[(i + 1)*N_LEN];
+    v[i*N_LEN] = v[(i*N_LEN)+N];
   }
-  u[0][N] = u[M][0];
-  v[M][0] = v[0][N];
+
+  u[N] = u[M*N_LEN];
+  v[M*N_LEN] = v[N];
+
   for (i=0;i<M_LEN;i++) {
     for (j=0;j<N_LEN;j++) {
-      uold[i][j] = u[i][j];
-      vold[i][j] = v[i][j];
-      pold[i][j] = p[i][j];
+      int idx = i*N_LEN+j;
+      uold[idx] = u[idx];
+      vold[idx] = v[idx];
+      pold[idx] = p[idx];
     }
   }
-     
+
   // Print initial values
   if ( L_OUT ) {
     printf(" number of points in the x direction %d\n", N); 
@@ -172,15 +197,15 @@ int main(int argc, char **argv) {
     mnmin = MIN(M,N);
     printf(" initial diagonal elements of p\n");
     for (i=0; i<mnmin; i++) {
-      printf("%f ",p[i][i]);
+      printf("%f ",p[i*N_LEN+i]);
     }
     printf("\n initial diagonal elements of u\n");
     for (i=0; i<mnmin; i++) {
-      printf("%f ",u[i][i]);
+      printf("%f ",u[i*N_LEN+i]);
     }
     printf("\n initial diagonal elements of v\n");
     for (i=0; i<mnmin; i++) {
-      printf("%f ",v[i][i]);
+      printf("%f ",v[i*N_LEN+i]);
     }
     printf("\n");
   }
@@ -201,23 +226,14 @@ int main(int argc, char **argv) {
 
     for (i=0;i<M;i++) {
       for (j=0;j<N;j++) {
-        cu[i + 1][j] = .5 * (p[i + 1][j] + p[i][j]) * u[i + 1][j];
-      }
-    }
-
-    for (i=0;i<M;i++) {
-      for (j=0;j<N;j++) {
-        cv[i][j + 1] = .5 * (p[i][j + 1] + p[i][j]) * v[i][j + 1];
-      }
-    }
-    for (i=0;i<M;i++) {
-      for (j=0;j<N;j++) {
-        z[i + 1][j + 1] = (fsdx * (v[i + 1][j + 1] - v[i][j + 1]) - fsdy * (u[i + 1][j + 1] - u[i + 1][j])) / (p[i][j] + p[i + 1][j] + p[i + 1][j + 1] + p[i][j + 1]);
-      }
-    }
-    for (i=0;i<M;i++) {
-      for (j=0;j<N;j++) {
-        h[i][j] = p[i][j] + .25 * (u[i + 1][j] * u[i + 1][j] + u[i][j] * u[i][j] + v[i][j + 1] * v[i][j + 1] + v[i][j] * v[i][j]);
+        int idx00 = (i*N_LEN) + j;
+        int idx01 = (i*N_LEN) + j+1;
+        int idx10 = ((i+1)*N_LEN) + j;
+        int idx11 = ((i+1)*N_LEN) + j+1;
+        cu[idx10] = .5 * (p[idx10] + p[idx00]) * u[idx10];
+        cv[idx01] = .5 * (p[idx01] + p[idx00]) * v[idx01];
+        z[idx11] = (fsdx * (v[idx11] - v[idx01]) - fsdy * (u[idx11] - u[idx10])) / (p[idx00] + p[idx10] + p[idx11] + p[idx01]);
+        h[idx00] = p[idx00] + .25 * (u[idx10] * u[idx10] + u[idx00] * u[idx00] + v[idx01] * v[idx01] + v[idx00] * v[idx00]);
       }
     }
 
@@ -225,24 +241,28 @@ int main(int argc, char **argv) {
     t100 = t100 + (c2 - c1); 
 
     // Periodic continuation
+
     for (j=0;j<N;j++) {
-      cu[0][j] = cu[M][j];
-      cv[M][j + 1] = cv[0][j + 1];
-      z[0][j + 1] = z[M][j + 1];
-      h[M][j] = h[0][j];
+      cu[j] = cu[M*N_LEN + j];
+      cv[M*N_LEN + j + 1] = cv[j + 1];
+      z[j + 1] = z[M*N_LEN + j + 1];
+      h[M*N_LEN +j] = h[j];
     }
+
     for (i=0;i<M;i++) {
-      cu[i + 1][N] = cu[i + 1][0];
-      cv[i][0] = cv[i][N];
-      z[i + 1][0] = z[i + 1][N];
-      h[i][N] = h[i][0];
+      cu[(i + 1)*N_LEN +N] = cu[(i + 1)*N_LEN];
+      cv[i*N_LEN] = cv[i*N_LEN + N];
+      z[(i + 1)*N_LEN] = z[i*N_LEN + N];
+      h[i*N_LEN + N] = h[i*N_LEN];
     }
-    cu[0][N] = cu[M][0];
-    cv[M][0] = cv[0][N];
-    z[0][0] = z[M][N];
-    h[M][N] = h[0][0];
+
+    cu[N] = cu[M*N_LEN];
+    cv[M*N_LEN] = cv[N];
+    z[0] = z[M*N_LEN+N];
+    h[M*N_LEN+N] = h[0];
      
     // Compute new values u,v and p
+
     tdts8 = tdt / 8.;
     tdtsdx = tdt / dx;
     tdtsdy = tdt / dy;
@@ -251,19 +271,13 @@ int main(int argc, char **argv) {
 
     for (i=0;i<M;i++) {
       for (j=0;j<N;j++) {
-        unew[i + 1][j] = uold[i + 1][j] + tdts8 * (z[i + 1][j + 1] + z[i + 1][j]) * (cv[i + 1][j + 1] + cv[i][j + 1] + cv[i][j] + cv[i + 1][j]) - tdtsdx * (h[i + 1][j] - h[i][j]);
-      }
-    }
-
-    for (i=0;i<M;i++) {
-      for (j=0;j<N;j++) {
-        vnew[i][j + 1] = vold[i][j + 1] - tdts8 * (z[i + 1][j + 1] + z[i][j + 1]) * (cu[i + 1][j + 1] + cu[i][j + 1] + cu[i][j] + cu[i + 1][j]) - tdtsdy * (h[i][j + 1] - h[i][j]);
-      }
-    }
-
-    for (i=0;i<M;i++) {
-      for (j=0;j<N;j++) {
-        pnew[i][j] = pold[i][j] - tdtsdx * (cu[i + 1][j] - cu[i][j]) - tdtsdy * (cv[i][j + 1] - cv[i][j]); 
+        int idx00 = (i*N_LEN) + j;
+        int idx01 = (i*N_LEN) + j+1;
+        int idx10 = ((i+1)*N_LEN) + j;
+        int idx11 = ((i+1)*N_LEN) + j+1;
+        unew[idx10] = uold[idx10] + tdts8 * (z[idx11] + z[idx10]) * (cv[idx11] + cv[idx01] + cv[idx00] + cv[idx10]) - tdtsdx * (h[idx10] - h[idx00]);
+        vnew[idx01] = vold[idx01] - tdts8 * (z[idx11] + z[idx01]) * (cu[idx11] + cu[idx01] + cu[idx00] + cu[idx10]) - tdtsdy * (h[idx01] - h[idx00]);
+        pnew[idx00] = pold[idx00] - tdtsdx * (cu[idx10] - cu[idx00]) - tdtsdy * (cv[idx01] - cv[idx00]);
       }
     }
 
@@ -273,20 +287,20 @@ int main(int argc, char **argv) {
     // Periodic continuation
 
     for (j=0;j<N;j++) {
-      unew[0][j] = unew[M][j];
-      vnew[M][j + 1] = vnew[0][j + 1];
-      pnew[M][j] = pnew[0][j];
+      unew[j] = unew[M*N_LEN+j];
+      vnew[M*N_LEN +j + 1] = vnew[j + 1];
+      pnew[M*N_LEN +j] = pnew[j];
     }
 
     for (i=0;i<M;i++) {
-      unew[i + 1][N] = unew[i + 1][0];
-      vnew[i][0] = vnew[i][N];
-      pnew[i][N] = pnew[i][0];
+      unew[(i + 1)*N_LEN+N] = unew[(i + 1)*N_LEN];
+      vnew[i*N_LEN] = vnew[i*N_LEN+N];
+      pnew[i*N_LEN+N] = pnew[i*N_LEN];
     }
 
-    unew[0][N] = unew[M][0];
-    vnew[M][0] = vnew[0][N];
-    pnew[M][N] = pnew[0][0];
+    unew[N] = unew[M*N_LEN];
+    vnew[M*N_LEN] = vnew[N];
+    pnew[M*N_LEN+N] = pnew[0];
 
     time = time + dt;
 
@@ -295,23 +309,12 @@ int main(int argc, char **argv) {
     if ( ncycle > 1 ) {
 
       c1 = wtime(); 
-
       for (i=0;i<M_LEN;i++) {
         for (j=0;j<N_LEN;j++) {
-          uold[i][j] = u[i][j] + alpha * (unew[i][j] - 2. * u[i][j] + uold[i][j]);
-        }
-      }
-
-      for (i=0;i<M_LEN;i++) {
-        for (j=0;j<N_LEN;j++) {
-          vold[i][j] = v[i][j] + alpha * (vnew[i][j] - 2. * v[i][j] + vold[i][j]);
-        }
-      }
-
-      for (i=0;i<M_LEN;i++) {
-        for (j=0;j<N_LEN;j++) {
-          pold[i][j] = p[i][j] + alpha * (pnew[i][j] - 2. * p[i][j] + pold[i][j]);
-
+          int idx = i*N_LEN+j;
+          uold[idx] = u[idx] + alpha * (unew[idx] - 2. * u[idx] + uold[idx]);
+          vold[idx] = v[idx] + alpha * (vnew[idx] - 2. * v[idx] + vold[idx]);
+          pold[idx] = p[idx] + alpha * (pnew[idx] - 2. * p[idx] + pold[idx]);
         }
       }
 
@@ -320,45 +323,35 @@ int main(int argc, char **argv) {
 #ifdef _COPY_
       for (i=0;i<M_LEN;i++) {
         for (j=0;j<N_LEN;j++) {
-          u[i][j] = unew[i][j];
-        }
-      }
-
-      for (i=0;i<M_LEN;i++) {
-        for (j=0;j<N_LEN;j++) {
-          v[i][j] = vnew[i][j];
-        }
-      }
-      for (i=0;i<M_LEN;i++) {
-        for (j=0;j<N_LEN;j++) {
-          p[i][j] = pnew[i][j];
+          int idx = i*N_LEN+j;
+          u[idx] = unew[idx];
+          v[idx] = vnew[idx];
+          p[idx] = pnew[idx];
         }
       }
 #else
       dswap( (double **)&u, (double **)&unew);
       dswap( (double **)&v, (double **)&vnew);
       dswap( (double **)&p, (double **)&pnew);
+
 #endif
 
       c2 = wtime(); 
       t300 = t300 + (c2 - c1);
      
     } else {
-      tdt = tdt + tdt;
 
-      for (i=0;i<M_LEN;i++) {
-        for (j=0;j<N_LEN;j++) {
-          uold[i][j] = u[i][j];
-          vold[i][j] = v[i][j];
-          pold[i][j] = p[i][j];
-          u[i][j] = unew[i][j];
-          v[i][j] = vnew[i][j];
-          p[i][j] = pnew[i][j];
-        }
-      }
+      tdt = tdt + tdt;
+      *uold = *u;
+      *vold = *v;
+      *pold = *p;
+      *u = *unew;
+      *v = *vnew;
+      *p = *pnew;
+   
 
     }
-  }
+  } // ** End of time loop ** 
 
   // have to swap values back for printing
 
@@ -366,23 +359,21 @@ int main(int argc, char **argv) {
   dswap( (double **)&v, (double **)&vnew);
   dswap( (double **)&p, (double **)&pnew);
 
-  // ** End of time loop ** 
-
   // Output p, u, v fields and run times.
   if (L_OUT) {
     ptime = time / 3600.;
     printf(" cycle number %d model time in hours %f\n", ITMAX, ptime);
     printf(" diagonal elements of p\n");
     for (i=0; i<mnmin; i++) {
-      printf("%f ",pnew[i][i]);
+      printf("%f ",pnew[i*N_LEN+i]);
     }
     printf("\n diagonal elements of u\n");
     for (i=0; i<mnmin; i++) {
-      printf("%f ",unew[i][i]);
+      printf("%f ",unew[i*N_LEN+i]);
     }
     printf("\n diagonal elements of v\n");
     for (i=0; i<mnmin; i++) {
-      printf("%f ",vnew[i][i]);
+      printf("%f ",vnew[i*N_LEN+i]);
     }
     printf("\n");
 
@@ -403,6 +394,21 @@ int main(int argc, char **argv) {
     printf(" time and megaflops for loop 200 %.6f %.6f\n", t200, mfs200);
     printf(" time and megaflops for loop 300 %.6f %.6f\n", t300, mfs300);
   }
+
+  free((void *) u);
+  free((void *) v);
+  free((void *) p);
+  free((void *) unew);
+  free((void *) vnew);
+  free((void *) pnew);
+  free((void *) uold);
+  free((void *) vold);
+  free((void *) pold);
+  free((void *) cu);
+  free((void *) cv);
+  free((void *) z);
+  free((void *) h);
+  free((void *) psi);
 
   return(0);
 }
