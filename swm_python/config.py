e058d57560552bd1fe81c372f548f464d7cd6d9b
Hannes Vogt <vogt@hey.com>
2024-02-29 22:08:20 +0100

    gt4py cartesian partially fused

--- swm_python/config.py
+++ swm_python/config.py
@@ -1,24 +1,27 @@
 import argparse
 
 parser = argparse.ArgumentParser(description="Shallow Water Model")
-parser.add_argument('--M', type=int, default=64, help='Number of points in the x direction')
-parser.add_argument('--N', type=int, default=64, help='Number of points in the y direction')
+parser.add_argument('--M', type=int, default=16, help='Number of points in the x direction')
+parser.add_argument('--N', type=int, default=16, help='Number of points in the y direction')
 parser.add_argument('--L_OUT', type=bool, default=True, help='a boolean for L_OUT')
-parser.add_argument('--ITMAX', type=int, default=4000, help='a boolean for L_OUT')
+parser.add_argument('--ITMAX', type=int, default=4000, help='Number of iterations')
+parser.add_argument('--VAL_DEEP', type=bool, default=True, help='Do deep validation')
+parser.add_argument('--backend', type=str, default='gt:cpu_ifirst', help='Backend to use: gt:cpu_ifirst, gt:cpu_kfirst, numpy, cuda, gt:gpu')
 
 
 args = parser.parse_args()
 
 # Initialize model parameters
+backend = args.backend
 M = args.M
 N = args.N
 M_LEN = M + 1
 N_LEN = N + 1
 L_OUT = args.L_OUT
-VAL=True
-VAL_DEEP=True
+VAL=False
+VAL_DEEP=False #args.VAL_DEEP
 VIS = False
-VIS_DT = 10
+VIS_DT = 100
 ITMAX = args.ITMAX
 dt = 90.
 dt = dt
--- swm_python/swm_cartesian.py
+++ swm_python/swm_cartesian.py
@@ -3,8 +3,8 @@ import numpy as np
 import gt4py.next as gtx
 import gt4py.cartesian.gtscript as gtscript
 from time import perf_counter
-#import cupy
-#import gt4py
+# import cupy
+# import gt4py
 import initial_conditions
 import utils
 import config
@@ -12,133 +12,71 @@ import config
 I = gtx.Dimension("I")
 J = gtx.Dimension("J")
 K = gtx.Dimension("K", kind = gtx.DimensionKind.VERTICAL)
-    
+
 dtype = np.float64
 
-cartesian_backend = "numpy"
-# cartesian_backend = "gt:gpu"
+cartesian_backend = config.backend
+allocator = gtx.gtfn_cpu
+if cartesian_backend in ("gt:gpu", "cuda", "dace:gpu"):
+    allocator = gtx.gtfn_gpu
+
+print(f"Using {cartesian_backend} backend with {allocator.__name__} allocator.")
 
 @gtscript.stencil(backend=cartesian_backend)
-def calc_h(
-    p: gtscript.Field[dtype],
-    u: gtscript.Field[dtype],
-    v: gtscript.Field[dtype],
-    h: gtscript.Field[dtype]
-):
+def calc_cucvzh(u: gtscript.Field[dtype], v: gtscript.Field[dtype], p: gtscript.Field[dtype], cu: gtscript.Field[dtype], cv: gtscript.Field[dtype], z: gtscript.Field[dtype], h: gtscript.Field[dtype], fsdx:float, fsdy: float):
     with computation(PARALLEL), interval(...):
-        h = p + 0.25 * (u[1,0,0] * u[1,0,0] + u * u + v[0,1,0] * v[0,1,0] + v * v)
+        cu = .5 * (p[1,0,0] + p) * u
+        cv = .5 * (p[0,1,0] + p) * v
+        z = (config.fsdx * (v[1,0,0] - v) - config.fsdy * (u[0,1,0] - u)) / (p[1,1,0] + p[0,1,0] + p + p[1,0,0])
+        h = p + 0.25 * (u[-1,0,0] * u[-1,0,0] + u * u + v[0,-1,0] * v[0,-1,0] + v * v)
 
 @gtscript.stencil(backend=cartesian_backend)
-def calc_z(
-    fsdx: float,
-    fsdy: float,
-    u: gtscript.Field[dtype],
-    v: gtscript.Field[dtype],
-    p: gtscript.Field[dtype],
-    z: gtscript.Field[dtype]
-):
-    with computation(PARALLEL), interval(...):
-        z = (fsdx * (v - v[-1,0,0]) - fsdy * (u - u[0,-1,0])) / (p[-1,-1,0] + p[0,-1,0] + p + p[-1,0,0])
-        
-@gtscript.stencil(backend=cartesian_backend)
-def calc_cu(
-    u: gtscript.Field[dtype],
-    p: gtscript.Field[dtype],
-    cu: gtscript.Field[dtype]
-):
-    with computation(PARALLEL), interval(...):
-        cu = .5 * (p + p[-1,0,0]) * u
-        
-@gtscript.stencil(backend=cartesian_backend)
-def calc_cv(
-    v: gtscript.Field[dtype],
-    p: gtscript.Field[dtype],
-    cv: gtscript.Field[dtype]
-):
-    with computation(PARALLEL), interval(...):
-        cv = .5 * (p[0,-1,0] + p) * v
-
-#recheck this section
-#pnew[i,j,0] = pold[i,j,0] - tdtsdx * (cu[i+1,j,0] - cu[i,j,0]) - tdtsdy * (cv[i,j+1,0] - cv[i,j,0])
-@gtscript.stencil(backend=cartesian_backend)
-def calc_pnew(
+def calc_uvp(
+    tdts8: float,
     tdtsdx: float,
     tdtsdy: float,
+    uold: gtscript.Field[dtype],
+    vold: gtscript.Field[dtype],
     pold: gtscript.Field[dtype],
     cu: gtscript.Field[dtype],
     cv: gtscript.Field[dtype],
+    z: gtscript.Field[dtype],
+    h: gtscript.Field[dtype],
+    unew: gtscript.Field[dtype],
+    vnew: gtscript.Field[dtype],
     pnew: gtscript.Field[dtype]
 ):
     with computation(PARALLEL), interval(...):
-        pnew = pold - tdtsdx * (cu[1,0,0] - cu) - tdtsdy * (cv[0,1,0] - cv)
+        unew = uold + tdts8 * (z + z[0,-1,0]) * (cv[1,0,0] + cv + cv[0,-1,0] + cv[1,-1,0]) - tdtsdx * (h[1,0,0] - h)
+        vnew = vold - tdts8 * (z + z[-1,0,0]) * (cu[-1,0,0] + cu[-1,1,0] + cu + cu[0,1,0]) - tdtsdy * (h[0,1,0] - h)
+        pnew = pold - tdtsdx * (cu- cu[-1,0,0]) - tdtsdy * (cv-cv[0,-1,0])
 
-#unew[i+1,j,0] = uold[i+1,j,0] + tdts8 * (z[i+1,j+1,0] + z[i+1,j,0]) * (cv[i+1,j+1,0] + cv[i+1,j,0] + cv[i,j+1,0] + cv[i,j,0]) - tdtsdx * (h[i+1,j,0] - h[i,j,0])
 @gtscript.stencil(backend=cartesian_backend)
-def calc_unew(
-    tdts8: float,
-    tdtsdx: float,
-    uold: gtscript.Field[dtype],
-    cu: gtscript.Field[dtype],
-    cv: gtscript.Field[dtype],
-    z: gtscript.Field[dtype],
-    h: gtscript.Field[dtype],
-    unew: gtscript.Field[dtype]
-):
-    with computation(PARALLEL), interval(...):
-        unew = uold + tdts8 * (z + z[0,1,0]) * (cv[0,1,0] + cv + cv[-1,1,0] + cv[-1,0,0]) - tdtsdx * (h - h[-1,0,0])
-
-    #vnew[i,j+1,0] = vold[i,j+1,0] - tdts8 * (z[i+1,j+1,0] + z[i,j+1,0]) * (cu[i+1,j+1,0] + cu[i+1,j,0] + cu[i,j+1,0] + cu[i,j,0]) - tdtsdy * (h[i,j+1,0] - h[i,j,0])    
-@gtscript.stencil(backend=cartesian_backend)
-def calc_vnew(
-    tdts8: float,
-    tdtsdy: float,
+def calc_uvp_old(
+    alpha: float,
+    v: gtscript.Field[dtype],
+    vnew: gtscript.Field[dtype],
     vold: gtscript.Field[dtype],
-    cu: gtscript.Field[dtype],
-    cv: gtscript.Field[dtype],
-    z: gtscript.Field[dtype],
-    h: gtscript.Field[dtype],
-    vnew: gtscript.Field[dtype]
-):
-    with computation(PARALLEL), interval(...):
-        vnew = vold - tdts8 * (z[1,0,0] + z) * (cu[1,0,0] + cu[1,-1,0] + cu + cu[0,-1,0]) - tdtsdy * (h - h[0,-1,0])
-        
-@gtscript.stencil(backend=cartesian_backend)
-def calc_pold(
-    p: gtscript.Field[dtype],
-    alpha: float,
-    pnew: gtscript.Field[dtype],
-    pold: gtscript.Field[dtype]
-):
-    with computation(PARALLEL), interval(...):
-        pold = p + alpha * (pnew - 2 * p + pold)
-
-@gtscript.stencil(backend=cartesian_backend)
-def calc_uold(
     u: gtscript.Field[dtype],
-    alpha: float,
     unew: gtscript.Field[dtype],
-    uold: gtscript.Field[dtype]
+    uold: gtscript.Field[dtype],
+    p: gtscript.Field[dtype],
+    pnew: gtscript.Field[dtype],
+    pold: gtscript.Field[dtype],
 ):
     with computation(PARALLEL), interval(...):
         uold = u + alpha * (unew - 2 * u + uold)
+        vold = v + alpha * (vnew - 2 * v + vold)
+        pold = p + alpha * (pnew - 2 * p + pold)
+
 
 @gtscript.stencil(backend=cartesian_backend)
-def calc_vold(
-    v: gtscript.Field[dtype],
-    alpha: float,
-    vnew: gtscript.Field[dtype],
-    vold: gtscript.Field[dtype]
-):
+def copy_3var(inp0: gtscript.Field[dtype], inp1: gtscript.Field[dtype], inp2: gtscript.Field[dtype], out0: gtscript.Field[dtype], out1: gtscript.Field[dtype], out2: gtscript.Field[dtype]):
     with computation(PARALLEL), interval(...):
-        vold = v + alpha * (vnew - 2 * v + vold)
-        
-@gtscript.stencil(backend=cartesian_backend)
-def copy_var(
-    inp: gtscript.Field[dtype],
-    out: gtscript.Field[dtype]
-):
-    with computation(PARALLEL), interval(...):
-        out = inp
+        out0 = inp0
+        out1 = inp1
+        out2 = inp2
+
 
 def main():
     dt0 = 0.
@@ -146,34 +84,40 @@ def main():
     dt2 = 0.
     dt3 = 0.
 
+    
+
     M_LEN = config.M_LEN
     N_LEN = config.N_LEN
     M = config.M
     N = config.N
     ITMAX = config.ITMAX
+    
+    _u, _v, _p = initial_conditions.initialize(M, N, config.dx, config.dy, config.a)
+    _u = _u[:,:,np.newaxis]
+    _v = _v[:,:,np.newaxis]
+    _p = _p[:,:,np.newaxis]
 
+    domain = gtx.domain({I:M+1, J:N+1, K:1})
 
-    unew = np.zeros((M_LEN, N_LEN, 1))
-    vnew = np.zeros((M_LEN, N_LEN, 1))
-    pnew = np.zeros((M_LEN, N_LEN, 1))
-    uold = np.zeros((M_LEN, N_LEN, 1))
-    vold = np.zeros((M_LEN, N_LEN, 1))
-    pold = np.zeros((M_LEN, N_LEN, 1))
-    cu = np.zeros((M_LEN, N_LEN, 1))
-    cv = np.zeros((M_LEN, N_LEN, 1))
-    z = np.zeros((M_LEN, N_LEN, 1))
-    h = np.zeros((M_LEN, N_LEN, 1))
+    h_gt = gtx.empty(domain,dtype=dtype,allocator=allocator)
+    z_gt = gtx.empty(domain,dtype=dtype,allocator=allocator)
+    cu_gt = gtx.empty(domain,dtype=dtype,allocator=allocator)
+    cv_gt = gtx.empty(domain,dtype=dtype,allocator=allocator)
+    pnew_gt = gtx.empty(domain,dtype=dtype,allocator=allocator)
+    unew_gt = gtx.empty(domain,dtype=dtype,allocator=allocator)
+    vnew_gt = gtx.empty(domain,dtype=dtype,allocator=allocator)
+    uold_gt = gtx.empty(domain,dtype=dtype,allocator=allocator)
+    vold_gt = gtx.empty(domain,dtype=dtype,allocator=allocator)
+    pold_gt = gtx.empty(domain,dtype=dtype,allocator=allocator)
 
-    u, v, p = initial_conditions.initialize(M, N, config.dx, config.dy, config.a)
-    u = u[:,:,np.newaxis]
-    v = v[:,:,np.newaxis]
-    p = p[:,:,np.newaxis]
-
+    u_gt = gtx.as_field(domain,_u,allocator=allocator)
+    v_gt = gtx.as_field(domain,_v,allocator=allocator)
+    p_gt = gtx.as_field(domain,_p,allocator=allocator)
 
     # Save initial conditions
-    uold = np.copy(u)
-    vold = np.copy(v)
-    pold = np.copy(p)
+    uold_gt[...] = u_gt[...]
+    vold_gt[...] = v_gt[...]
+    pold_gt[...] = p_gt[...]
 
     # Print initial conditions
     if config.L_OUT:
@@ -183,140 +127,131 @@ def main():
         print(" grid spacing in the y direction: ", config.dy)
         print(" time step: ", config.dt)
         print(" time filter coefficient: ", config.alpha)
-        print(" Initial p:\n", p[:,:,0].diagonal()[:-1])
-        print(" Initial u:\n", u[:,:,0].diagonal()[:-1])
-        print(" Initial v:\n", v[:,:,0].diagonal()[:-1])
+        print(" Initial p:\n", p_gt.asnumpy()[:,:,0].diagonal()[:-1])
+        print(" Initial u:\n", u_gt.asnumpy()[:,:,0].diagonal()[:-1])
+        print(" Initial v:\n", v_gt.asnumpy()[:,:,0].diagonal()[:-1])
 
-    nx = M
-    ny = N
-    nz = 1
-    gt4py_type = "cartesian"
-    #gt4py_type = "next"
-    allocator = gtx.itir_python
-    # allocator = gtx.gtfn_gpu
-
-    domain = gtx.domain({I:nx+1, J:ny+1, K:nz})
-
-    h_gt = gtx.as_field(domain,h,allocator=allocator)
-    z_gt = gtx.as_field(domain,z,allocator=allocator)
-    cu_gt = gtx.as_field(domain,cu,allocator=allocator)
-    cv_gt = gtx.as_field(domain,cv,allocator=allocator)
-    pnew_gt = gtx.as_field(domain,pnew,allocator=allocator)
-    unew_gt = gtx.as_field(domain,unew,allocator=allocator)
-    vnew_gt = gtx.as_field(domain,vnew,allocator=allocator)
-    uold_gt = gtx.as_field(domain,uold,allocator=allocator)
-    vold_gt = gtx.as_field(domain,vold,allocator=allocator)
-    pold_gt = gtx.as_field(domain,pold,allocator=allocator)
 
 
     t0_start = perf_counter()
     time = 0.0
     tdt = config.dt
+
+    u_origin=(1,0,0)
+    v_origin=(0,1,0)
+    p_origin=(0,0,0)
+    z_origin=(1,1,0)
     # Main time loop
     for ncycle in range(ITMAX):
-        p_gt = gtx.as_field(domain,p,allocator=allocator)
-        u_gt = gtx.as_field(domain,u,allocator=allocator)
-        v_gt = gtx.as_field(domain,v,allocator=allocator)
 
         if((ncycle%100==0) & (config.VIS==False)):
-            print(f"cycle number{ncycle} and gt4py type {gt4py_type}")
-        
+            print(f"cycle number{ncycle} and gt4py type cartesian")
+
         if config.VAL_DEEP and ncycle <= 3:
-            utils.validate_uvp(u, v, p, M, N, ncycle, 'init')
             utils.validate_uvp(u_gt.asnumpy(), v_gt.asnumpy(), p_gt.asnumpy(), M, N, ncycle, 'init')
-        
+
         t1_start = perf_counter()
-        # Calculate cu, cv, z, and h
-        calc_h(p=p_gt, u=u_gt, v=v_gt, h=h_gt, origin=(0,0,0), domain=(nx,ny,nz)) 
-        h = h_gt.asnumpy()
 
-        calc_z(fsdx=config.fsdx, fsdy=config.fsdy, u=u_gt, v=v_gt, p=p_gt, z=z_gt, origin=(1,1,0), domain=(nx,ny,nz)) # domain(nx+1,ny+1,nz) gives error why?
-        z = z_gt.asnumpy()
+        calc_cucvzh(
+            u=u_gt,
+            v=v_gt,
+            p=p_gt,
+            cu=cu_gt,
+            cv=cv_gt,
+            z=z_gt,
+            h=h_gt,
+            fsdx=config.fsdx,
+            fsdy=config.fsdy,
+            origin={"u":u_origin, "v":v_origin, "p":p_origin, "z":z_origin, "h":p_origin, "cu":u_origin, "cv":v_origin},
+            domain=(M, N, 1),
+        )
 
-        calc_cu(u=u_gt, p=p_gt, cu=cu_gt, origin=(1,0,0), domain=(nx,ny,nz)) # (nx,ny+1,nz)-->works domain(nx+1,ny+1,nz) gives error why? try removing ny+1
-        cu = cu_gt.asnumpy()
-
-        calc_cv(v=v_gt, p=p_gt, cv=cv_gt, origin=(0,1,0), domain=(nx,ny,nz)) #(nx+1,ny,nz)--> works domain(nx+1,ny+1,nz) gives error why?
-        cv = cv_gt.asnumpy()
         t1_stop = perf_counter()
         dt1 = dt1 + (t1_stop - t1_start)
         # # Periodic Boundary conditions
-        #try region
-        cu[0, :,0] = cu[M, :,0]
-        h[M, :,0] = h[0, :,0]
-        cv[M, 1:,0] = cv[0, 1:,0]
-        z[0, 1:,0] = z[M, 1:,0]
-        
-        cv[:, 0,0] = cv[:, N,0]
-        h[:, N,0] = h[:, 0,0]
-        cu[1:, N,0] = cu[1:, 0,0]
-        z[1:, 0,0] = z[1:, N,0]
-            
-        cu[0, N,0] = cu[M, 0,0]
-        cv[M, 0,0] = cv[0, N,0]
-        z[0, 0,0] = z[M, N,0]
-        h[M, N,0] = h[0, 0,0]
-            
+        # try region
+        cu_gt[0, :,0] = cu_gt[M, :,0]
+        # update_boundary(cu_gt, cu_gt, M, N)
+
+        h_gt[M, :,0] = h_gt[0, :,0]
+        cv_gt[M, 1:,0] = cv_gt[0, 1:,0]
+        z_gt[0, 1:,0] = z_gt[M, 1:,0]
+
+        cv_gt[:, 0,0] = cv_gt[:, N,0]
+        h_gt[:, N,0] = h_gt[:, 0,0]
+        cu_gt[1:, N,0] = cu_gt[1:, 0,0]
+        z_gt[1:, 0,0] = z_gt[1:, N,0]
+
+        cu_gt[0, N,0] = cu_gt[M, 0,0]
+        cv_gt[M, 0,0] = cv_gt[0, N,0]
+        z_gt[0, 0,0] = z_gt[M, N,0]
+        h_gt[M, N,0] = h_gt[0, 0,0]
+
         if config.VAL_DEEP and ncycle <=1:
-            utils.validate_cucvzh(cu, cv, z, h, M, N, ncycle, 't100')
-            
+            utils.validate_cucvzh(cu_gt.asnumpy(), cv_gt.asnumpy(), z_gt.asnumpy(), h_gt.asnumpy(), M, N, ncycle, 't100')
+
         # Calclulate new values of u,v, and p
         tdts8 = tdt / 8.
         tdtsdx = tdt / config.dx
         tdtsdy = tdt / config.dy
-        #print(tdts8, tdtsdx, tdtsdy)
+        # print(tdts8, tdtsdx, tdtsdy)
 
         t2_start = perf_counter()
-        calc_unew(tdts8=tdts8, tdtsdx=tdtsdx, uold=uold_gt, cu=cu_gt, cv=cv_gt, z=z_gt, h=h_gt, unew=unew_gt, origin=(1,0,0), domain=(nx,ny,nz))
-        unew = unew_gt.asnumpy()
         
-        calc_vnew(tdts8=tdts8, tdtsdy=tdtsdy, vold=vold_gt, cu=cu_gt, cv=cv_gt, z=z_gt, h=h_gt, vnew=vnew_gt, origin=(0,1,0), domain=(nx,ny,nz))
-        vnew = vnew_gt.asnumpy()
-        
-        calc_pnew(tdtsdx=tdtsdx, tdtsdy=tdtsdy, pold=pold_gt, cu=cu_gt, cv=cv_gt, pnew=pnew_gt, origin=(0,0,0), domain=(nx,ny,nz))
-        pnew = pnew_gt.asnumpy()
+        calc_uvp(
+            tdts8=tdts8,
+            tdtsdx=tdtsdx,
+            tdtsdy=tdtsdy,
+            uold=uold_gt,
+            vold=vold_gt,
+            pold=pold_gt,
+            cu=cu_gt,
+            cv=cv_gt,
+            z=z_gt,
+            h=h_gt,
+            unew=unew_gt,
+            vnew=vnew_gt,
+            pnew=pnew_gt,
+            origin={
+                "uold": u_origin,
+                "vold": v_origin,
+                "pold": p_origin,
+                "cu": u_origin,
+                "cv": v_origin,
+                "z": z_origin,
+                "h": p_origin,
+                "unew": u_origin,
+                "vnew": v_origin,
+                "pnew": p_origin,
+            },
+            domain=(M, N, 1),
+        )
+
         t2_stop = perf_counter()
         dt2 = dt2 + (t2_stop - t2_start)
-            
-        # for i in range(M):
-        #     for j in range(N):
-        #         unew[i+1,j,0] = uold[i+1,j,0] + tdts8 * (z[i+1,j+1,0] + z[i+1,j,0]) * (cv[i+1,j+1,0] + cv[i+1,j,0] + cv[i,j+1,0] + cv[i,j,0]) - tdtsdx * (h[i+1,j,0] - h[i,j,0])
-        #         vnew[i,j+1,0] = vold[i,j+1,0] - tdts8 * (z[i+1,j+1,0] + z[i,j+1,0]) * (cu[i+1,j+1,0] + cu[i+1,j,0] + cu[i,j+1,0] + cu[i,j,0]) - tdtsdy * (h[i,j+1,0] - h[i,j,0])
-        #         pnew[i,j,0] = pold[i,j,0] - tdtsdx * (cu[i+1,j,0] - cu[i,j,0]) - tdtsdy * (cv[i,j+1,0] - cv[i,j,0])
-                            
+
         # Periodic Boundary conditions
-        unew[0, :,0] = unew[M, :,0]
-        pnew[M, :,0] = pnew[0, :,0]
-        vnew[M, 1:,0] = vnew[0, 1:,0]
-        unew[1:, N,0] = unew[1:, 0,0]
-        vnew[:, 0,0] = vnew[:, N,0]
-        pnew[:, N,0] = pnew[:, 0,0]
-        
-        unew[0, N,0] = unew[M, 0,0]
-        vnew[M, 0,0] = vnew[0, N,0]
-        pnew[M, N,0] = pnew[0, 0,0]
-        
+        unew_gt[0, :,0] = unew_gt[M, :,0]
+        pnew_gt[M, :,0] = pnew_gt[0, :,0]
+        vnew_gt[M, 1:,0] = vnew_gt[0, 1:,0]
+        unew_gt[1:, N,0] = unew_gt[1:, 0,0]
+        vnew_gt[:, 0,0] = vnew_gt[:, N,0]
+        pnew_gt[:, N,0] = pnew_gt[:, 0,0]
+
+        unew_gt[0, N,0] = unew_gt[M, 0,0]
+        vnew_gt[M, 0,0] = vnew_gt[0, N,0]
+        pnew_gt[M, N,0] = pnew_gt[0, 0,0]
+
         if config.VAL_DEEP and ncycle <= 1:
-            utils.validate_uvp(unew, vnew, pnew, M, N, ncycle, 't200')
-        
+            utils.validate_uvp(unew_gt.asnumpy(), vnew_gt.asnumpy(), pnew_gt.asnumpy(), M, N, ncycle, 't200')
+
         time = time + config.dt
 
         if(ncycle > 0):
             t3_start = perf_counter()
-            calc_pold(p=p_gt, alpha=config.alpha, pnew=pnew_gt, pold=pold_gt, origin=(0,0,0), domain=(nx+1,ny+1,nz))
-            pold = pold_gt.asnumpy()
-            calc_uold(u=u_gt, alpha=config.alpha, unew=unew_gt, uold=uold_gt, origin=(0,0,0), domain=(nx+1,ny+1,nz))
-            uold = uold_gt.asnumpy()
-            calc_vold(v=v_gt, alpha=config.alpha, vnew=vnew_gt, vold=vold_gt, origin=(0,0,0), domain=(nx+1,ny+1,nz))
-            vold = vold_gt.asnumpy()
-            
-            copy_var(unew_gt, u_gt, origin=(0,0,0), domain=(nx+1,ny+1,nz))
-            copy_var(vnew_gt, v_gt, origin=(0,0,0), domain=(nx+1,ny+1,nz))
-            copy_var(pnew_gt, p_gt, origin=(0,0,0), domain=(nx+1,ny+1,nz))
+            calc_uvp_old(alpha=config.alpha, v=v_gt, vnew=vnew_gt, vold=vold_gt, u=u_gt, unew=unew_gt, uold=uold_gt, p=p_gt, pnew=pnew_gt, pold=pold_gt, domain=(M+1, N+1, 1))
 
-            u = u_gt.asnumpy()
-            v = v_gt.asnumpy()
-            p = p_gt.asnumpy()
+            copy_3var(unew_gt, vnew_gt, pnew_gt, u_gt, v_gt, p_gt, origin=(0,0,0), domain=(M+1,N+1,1))
 
             t3_stop = perf_counter()
             dt3 = dt3 + (t3_stop - t3_start)
@@ -324,31 +259,31 @@ def main():
         else:
             tdt = tdt+tdt
 
-            uold = np.copy(u[...])
-            vold = np.copy(v[...])
-            pold = np.copy(p[...])
-            u = np.copy(unew[...])
-            v = np.copy(vnew[...])
-            p = np.copy(pnew[...])
+            uold_gt[...] = u_gt[...]
+            vold_gt[...] = v_gt[...]
+            pold_gt[...] = p_gt[...]
+            u_gt[...] = unew_gt[...]
+            v_gt[...] = vnew_gt[...]
+            p_gt[...] = pnew_gt[...]
 
         if((config.VIS == True) & (ncycle%config.VIS_DT==0)):
-            utils.live_plot3(u, v, p, "ncycle: " + str(ncycle))
-            
+            utils.live_plot3(u_gt.asnumpy(), v_gt.asnumpy(), p_gt.asnumpy(), "ncycle: " + str(ncycle))
+
     t0_stop = perf_counter()
     dt0 = dt0 + (t0_stop - t0_start)
     # Print initial conditions
     if config.L_OUT:
-            print("cycle number ", ITMAX)
-            print(" diagonal elements of p:\n", pnew[:,:,0].diagonal()[:-1])
-            print(" diagonal elements of u:\n", unew[:,:,0].diagonal()[:-1])
-            print(" diagonal elements of v:\n", vnew[:,:,0].diagonal()[:-1])
+        print("cycle number ", ITMAX)
+        print(" diagonal elements of p:\n", pnew_gt.asnumpy()[:,:,0].diagonal()[:-1])
+        print(" diagonal elements of u:\n", unew_gt.asnumpy()[:,:,0].diagonal()[:-1])
+        print(" diagonal elements of v:\n", vnew_gt.asnumpy()[:,:,0].diagonal()[:-1])
     print("total: ",dt0)
     print("t100: ",dt1)
     print("t200: ",dt2)
     print("t300: ",dt3)
 
     if config.VAL:
-        utils.final_validation(u, v, p, ITMAX=ITMAX, M=M, N=N)
+        utils.final_validation(u_gt.asnumpy(), v_gt.asnumpy(), p_gt.asnumpy(), ITMAX=ITMAX, M=M, N=N)
 
 if __name__ == "__main__":
     main()
