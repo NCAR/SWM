52a20d8a3813ccdc11c7a1ede99cb36494fce795
John Dennis <dennis@Jucar.edu>
2024-04-26 13:06:28 -0600

    Optimized julia version of code.  This is still not correct.

--- swm_julia/swm_julia.jl
+++ swm_julia/swm_julia.jl
@@ -1,12 +1,13 @@
 using Profile
+using Printf
 using LinearAlgebra
 using LoopVectorization
 
-const M::Int = 256
-const N::Int = 256
+const M::Int = 16
+const N::Int = 16
 const M_LEN::Int = M + 1
 const N_LEN::Int = N + 1
-const ITMAX::Int = 40
+const ITMAX::Int = 4
 
 const dt::Float64 = 90.::Float64
 const dx::Float64 = 100000.::Float64
@@ -16,6 +17,55 @@ const fsdy::Float64 = 4.::Float64 / (dy)
 const a::Float64 = 1000000.::Float64
 const alpha::Float64 = 0.001::Float64
 L_OUT = false
+
+function meminfo_julia()
+  # @printf "GC total:  %9.3f MiB\n" Base.gc_total_bytes(Base.gc_num())/2^20
+  # Total bytes (above) usually underreports, thus I suggest using live bytes (below)
+  @printf "GC live:   %9.3f MiB\n" Base.gc_live_bytes()/2^20
+  @printf "JIT:       %9.3f MiB\n" Base.jit_total_bytes()/2^20
+  @printf "Max. RSS:  %9.3f MiB\n" Sys.maxrss()/2^20
+end
+
+
+function validate_uvp(u::Array{Float64}, v::Array{Float64}, p::Array{Float64}, M::Int, N::Int, step::Int, suffix::String)
+    u_ref, v_ref, p_ref = read_uvp(step, suffix, M, N)
+    # a = norm((u.-u_ref),Inf)
+    println("U_ref - U: ",norm((u.-u_ref),Inf))
+    println("U_ref: ",u_ref[1,1]);
+    println("U[1,1]: ",u[1,1]);
+    println("U[1,N]: ",u[1,N]);
+    println("U[M,1]: ",u[M,1]);
+    println("U[M,N]: ",u[M,N]);
+
+    #println("V_ref - V: ",norm((v.-v_ref),Inf))
+    #println("P_ref - P: ",norm((p.-p_ref),Inf))
+    # np.testing.assert_allclose(u, u_ref)
+    # np.testing.assert_allclose(v, v_ref)
+    # np.testing.assert_allclose(p, p_ref)
+    # print("step {step} {suffix} values are correct.\n")
+end
+
+function read_uvp(step::Int, suffix::String, M::Int, N::Int)
+   
+    base = "../ref/" * string(M) * "x" * string(N) * "/"
+    step = "step" * string(step) * "." * suffix * ".bin"
+    u_file = base * "u." * step;
+    v_file = base * "v." * step;
+    p_file = base * "p." * step;
+
+    # u_file = f'../ref/{M}x{N}/u.step{step}.{suffix}.bin'
+    # v_file = f'../ref/{M}x{N}/v.step{step}.{suffix}.bin'
+    # p_file = f'../ref/{M}x{N}/p.step{step}.{suffix}.bin'
+
+    u = zeros(Float64,M+1,M+1)
+    v = zeros(Float64,M+1,M+1)
+    p = zeros(Float64,M+1,M+1)
+
+    read!(u_file,u)
+    read!(v_file,v)
+    read!(p_file,p)
+    return u, v, p
+end
 
 
 function initialize(u, v, p, M, N, dx, dy, a)
@@ -83,14 +133,6 @@ if L_OUT
     println(" Initial v:\n", diag(v)[1:end-1])
 end
 
-function turbo_dot(x::AbstractVector{Float64}, A::AbstractMatrix{Float64}, y::AbstractVector{Float64})
-    s = 0.0;
-    @turbo for j in eachindex(y), i in eachindex(x)
-         s += x[i]*A[i,j]*y[j];
-    end
-    return s;
-end
-
 function calc_cu!(p::Array{Float64},u::Array{Float64},cu::Array{Float64})
    
    # cu[2:end, 1:end-1] = ((p[2:end, 1:end-1] .+ p[1:end-1,1:end-1]) .* u[2:end,1:end-1]) ./ 2.0::Float64
@@ -108,185 +150,176 @@ function calc_cv!(p::Array{Float64},v::Array{Float64},cv::Array{Float64})
    return
 end
 function calc_z!(v::Array{Float64},u::Array{Float64},p::Array{Float64},z::Array{Float64},fsdx::Float64,fsdy::Float64)
-     # z[2:end, 2:end] = (fsdx .* (v[2:end, 2:end] .- v[1:end-1, 2:end]) .- 
-     #                  fsdy .* (u[2:end, 2:end] .- u[2:end, 1:end-1])) ./
-     #                  (p[1:end-1,1:end-1] .+ p[2:end,1:end-1] .+ 
-     #                 p[2:end,2:end] .+ p[1:end-1,2:end])
-     @turbo for j=2:N,i=2:M
-       @inbounds z[i,j] = (fsdx * (v[i,j] - v[i-1,j]) - fsdy * (u[i,j]-u[i,j-1]))/
-			(p[i-1,j-1] + p[i,j-1] + p[i,j] + p[i-1,j])
-     end
-     return
+   # z[2:end, 2:end] = (fsdx .* (v[2:end, 2:end] .- v[1:end-1, 2:end]) .- 
+   #                  fsdy .* (u[2:end, 2:end] .- u[2:end, 1:end-1])) ./
+   #                  (p[1:end-1,1:end-1] .+ p[2:end,1:end-1] .+ 
+   #                 p[2:end,2:end] .+ p[1:end-1,2:end])
+   @turbo for j=2:N,i=2:M
+      @inbounds z[i,j] = (fsdx * (v[i,j] - v[i-1,j]) - fsdy * (u[i,j]-u[i,j-1]))/
+   	   		(p[i-1,j-1] + p[i,j-1] + p[i,j] + p[i-1,j])
+   end
+   return
 end
 function calc_h!(p::Array{Float64},u::Array{Float64},v::Array{Float64},h::Array{Float64})
-     # h[1:end-1,1:end-1] = p[1:end-1,1:end-1] .+ 0.25::Float64 .* 
-     #                        (u[2:end, 1:end-1] .* u[2:end, 1:end-1] .+
-     #                        u[1:end-1,1:end-1] .* u[1:end-1,1:end-1] .+ 
-     #                        v[1:end-1, 2:end] .* v[1:end-1, 2:end] .+ 
-     #                       v[1:end-1,1:end-1] .* v[1:end-1,1:end-1])
-     @turbo for j=1:N,i=1:M
-       @inbounds h[i,j] = p[i,j] + 0.25* (u[i+1,j]^2 + u[i,j]^2 + v[i,j+2]^2 + v[i,j]^2)
-     end
+   # h[1:end-1,1:end-1] = p[1:end-1,1:end-1] .+ 0.25::Float64 .* 
+   #                        (u[2:end, 1:end-1] .* u[2:end, 1:end-1] .+
+   #                        u[1:end-1,1:end-1] .* u[1:end-1,1:end-1] .+ 
+   #                        v[1:end-1, 2:end] .* v[1:end-1, 2:end] .+ 
+   #                       v[1:end-1,1:end-1] .* v[1:end-1,1:end-1])
+   @turbo for j=1:N,i=1:M
+      @inbounds h[i,j] = p[i,j] + 0.25* (u[i+1,j]^2 + u[i,j]^2 + v[i,j+2]^2 + v[i,j]^2)
+   end
 end
-function calc_unew!(uold::Array{Float64},z::Array{Float64},cv::Array{Float64},h::Array{Float64},unew::Array{Float64},tdts8,tdtsdx::Float64)
-        #unew[2:end,1:end-1]= uold[2:end,1:end-1] .+ tdts8 .* 
-        #                        (z[2:end, 2:end] .+ z[2:end, 1:end-1]) .*
-        #                        (cv[2:end,2:end] .+ cv[2:end,1:end-1] .+ 
-        #                        cv[1:end-1,2:end] .+ cv[1:end-1,1:end-1]) .-
-        #                        tdtsdx .* (h[2:end, 1:end-1] .- h[1:end-1,1:end-1])
-        @turbo for j=2:N+1,i=1:M
-          @inbounds unew[i,j] = uold[i,j] + tdts8 * (z[i+1,j] + z[i+1,j-1]) * 
-				(cv[i+1,j] + cv[i+1,j-1] + cv[i,j] + cv[i,j-1]) -
-				tdtsdx * (h[i+1,j-1]-h[i,j-1])				
-	end
+function calc_unew!(uold::Array{Float64},z::Array{Float64},cv::Array{Float64},h::Array{Float64},unew::Array{Float64},tdts8::Float64,tdtsdx::Float64)
+   #unew[2:end,1:end-1]= uold[2:end,1:end-1] .+ tdts8 .* 
+   #                        (z[2:end, 2:end] .+ z[2:end, 1:end-1]) .*
+   #                        (cv[2:end,2:end] .+ cv[2:end,1:end-1] .+ 
+   #                        cv[1:end-1,2:end] .+ cv[1:end-1,1:end-1]) .-
+   #                        tdtsdx .* (h[2:end, 1:end-1] .- h[1:end-1,1:end-1])
+   @turbo for j=2:N+1,i=1:M
+      @inbounds unew[i,j] = uold[i,j] + tdts8 * (z[i+1,j] + z[i+1,j-1]) * 
+			(cv[i+1,j] + cv[i+1,j-1] + cv[i,j] + cv[i,j-1]) -
+			tdtsdx * (h[i+1,j-1]-h[i,j-1])				
+   end
 end
 function calc_vnew!(vold::Array{Float64},z::Array{Float64},cu::Array{Float64},h::Array{Float64},vnew::Array{Float64},tdts8::Float64,tdtsdy::Float64)
-        # vnew[1:end-1,2:end] = vold[1:end-1,2:end] .- tdts8 .*
-        #                         (z[2:end, 2:end] .+ z[1:end-1, 2:end]) .*
-        #                        (cu[2:end,2:end] .+ cu[2:end,1:end-1] .+ 
-        #                        cu[1:end-1,2:end] .+ cu[1:end-1,1:end-1]) .-
-        #                        tdtsdy .* (h[1:end-1, 2:end] .- h[1:end-1,1:end-1])
-	@turbo for j=2:N+1,i=1:M
-	   @inbounds vnew[i,j] = vold[i,j] - tdts8 * (z[i+1,j] + z[i,j]) *
-                                 (cu[i+1,j] + cu[i+1,j-1] + cu[i,j] + cu[i,j-1]) -
-				 tdtsdy * (h[i,j] - h[i,j-1])
-	end
+   # vnew[1:end-1,2:end] = vold[1:end-1,2:end] .- tdts8 .*
+   #                         (z[2:end, 2:end] .+ z[1:end-1, 2:end]) .*
+   #                        (cu[2:end,2:end] .+ cu[2:end,1:end-1] .+ 
+   #                        cu[1:end-1,2:end] .+ cu[1:end-1,1:end-1]) .-
+   #                        tdtsdy .* (h[1:end-1, 2:end] .- h[1:end-1,1:end-1])
+   @turbo for j=2:N+1,i=1:M
+      @inbounds vnew[i,j] = vold[i,j] - tdts8 * (z[i+1,j] + z[i,j]) *
+                           (cu[i+1,j] + cu[i+1,j-1] + cu[i,j] + cu[i,j-1]) -
+			 tdtsdy * (h[i,j] - h[i,j-1])
+   end
 end
+function calc_uvpnew!(uold::Array{Float64},
+		     vold::Array{Float64},
+		     pold::Array{Float64},
+		       cu::Array{Float64},
+		       cv::Array{Float64},
+			z::Array{Float64},
+			h::Array{Float64},
+	     	     unew::Array{Float64},
+	     	     vnew::Array{Float64},
+	     	     pnew::Array{Float64},
+		    tdts8::Float64,tdtsdy::Float64)
+   @turbo for j=2:N+1,i=1:M
+      @inbounds unew[i,j] = uold[i,j] + tdts8 * (z[i+1,j] + z[i+1,j-1]) * 
+			(cv[i+1,j] + cv[i+1,j-1] + cv[i,j] + cv[i,j-1]) -
+			tdtsdx * (h[i+1,j-1]-h[i,j-1])				
+      @inbounds vnew[i,j] = vold[i,j] - tdts8 * (z[i+1,j] + z[i,j]) *
+                           (cu[i+1,j] + cu[i+1,j-1] + cu[i,j] + cu[i,j-1]) -
+			 tdtsdy * (h[i,j] - h[i,j-1])
+   end
+   @turbo for j=1:N,i=1:M
+      @inbounds pnew[i,j] = pold[i,j] - tdtsdx * (cu[i+2,j] - cu[i,j]) -
+			 tdtsdy * ( cv[i,j+1] - cv[i,j])
+   end
+end
+
 function calc_pnew!(pold::Array{Float64},cu::Array{Float64},cv::Array{Float64},pnew::Array{Float64},tdtsdx::Float64)
-        # pnew[1:end-1,1:end-1] = pold[1:end-1,1:end-1] .- tdtsdx .*
-        #                         (cu[2:end, 1:end-1] .- cu[1:end-1,1:end-1]) .-
-        #                         tdtsdy .* (cv[1:end-1, 2:end] .- cv[1:end-1,1:end-1])
-	@turbo for j=1:N,i=1:M
- 	   @inbounds pnew[i,j] = pold[i,j] - tdtsdx * (cu[i+2,j] - cu[i,j]) -
-				 tdtsdy * ( cv[i,j+1] - cv[i,j])
-        end
-end
-function bndry1!(h::Array{Float64},z::Array{Float64},cu::Array{Float64},cv::Array{Float64})
-    @inbounds cu[1,1]  = cu[M, 1]
-    @inbounds h[M,1]   = h[1, 1]
-    @turbo for i=2:N+1
-       @inbounds cu[1,i]  = cu[M, i]
-       @inbounds h[M,i]   = h[1, i]
-       @inbounds cv[M, i] = cv[1, i]
-       @inbounds z[1, i]  = z[M, i]
-    end
+   # pnew[1:end-1,1:end-1] = pold[1:end-1,1:end-1] .- tdtsdx .*
+   #                         (cu[2:end, 1:end-1] .- cu[1:end-1,1:end-1]) .-
+   #                         tdtsdy .* (cv[1:end-1, 2:end] .- cv[1:end-1,1:end-1])
+   @turbo for j=1:N,i=1:M
+      @inbounds pnew[i,j] = pold[i,j] - tdtsdx * (cu[i+2,j] - cu[i,j]) -
+			 tdtsdy * ( cv[i,j+1] - cv[i,j])
+   end
 end
 
-#@views cenU(A) = A[2:end,1:end-1]
-#@views cenV(A) = A[1:end-1,2:end]
-#@views cenP(A) = A[1:end-1,1:end-1]
-#@views cenZ(A) = A[2:end,2:end]
-
-#@views im1U(A) = A[1:end-1,1:end-1]
-#@views jm1V(A) = A[1:end-1,1:end-1]
-#@views allJ(n,A) = A[n,1:end]
-#@views allI(n,A) = A[1:end,n]
-
+@views d_yi(A) = A[2:end, 2:end] .- A[2:end, 1:end-1]
+@views d_xi(A) = A[2:end, 2:end] .- A[1:end-1, 2:end]
+@views dp(A) = p[1:end-1,1:end-1] .+ A[2:end,1:end-1] .+ A[2:end,2:end] .+ A[1:end-1,2:end]
 
 global tdt = dt
 @time begin
 for ncycle in 1:ITMAX
-    global alpha, fsdx, fsdy
-    global p, u, v, cu, cv, z, h, uold, vold, pold, unew, vnew, pnew, tdt, tdtsdx, tdtsdy, time, tdts
-    
-     #
-     # cu[2:end, 1:end-1] = ((p[2:end, 1:end-1] .+ p[1:end-1,1:end-1]) .* u[2:end,1:end-1]) ./ 2.0::Float64
-     calc_cu!(p,u,cu)
-     #
-     # cv[1:end-1, 2:end] = ((p[1:end-1, 2:end] .+ p[1:end-1,1:end-1]) .* v[1:end-1,2:end]) ./ 2.0::Float64
-     calc_cv!(p,v,cv)
-     #
-     # end
-
-
-
-     # z[2:end, 2:end] = (fsdx .* (v[2:end, 2:end] .- v[1:end-1, 2:end]) .- 
-     #                 fsdy .* (u[2:end, 2:end] .- u[2:end, 1:end-1])) ./
-     #                 (p[1:end-1,1:end-1] .+ p[2:end,1:end-1] .+ 
-     #                 p[2:end,2:end] .+ p[1:end-1,2:end])
-     calc_z!(v,u,p,z,fsdx,fsdy)
-
-    
-     # h[1:end-1,1:end-1] = p[1:end-1,1:end-1] .+ 0.25::Float64 .* 
-     #                        (u[2:end, 1:end-1] .* u[2:end, 1:end-1] .+
-     #                        u[1:end-1,1:end-1] .* u[1:end-1,1:end-1] .+ 
-     #                        v[1:end-1, 2:end] .* v[1:end-1, 2:end] .+ 
-     #                       v[1:end-1,1:end-1] .* v[1:end-1,1:end-1])
-     calc_h!(p,u,v,h)
+   global alpha, fsdx, fsdy,tdts8
+   global p, u, v, cu, cv, z, h, uold, vold, pold, unew, vnew, pnew, tdt, tdtsdx, tdtsdy, time, tdts
     
 
-    # bndry1!(h,z,cu,cv)
-    cu[1,1:N+1]  = cu[M, 1:N+1]
-     h[M,1:N+1]  =  h[1, 1:N+1]
-    cv[M, 2:N+1] = cv[1, 2:N+1]
-     z[M, 2:N+1] =  z[1, 2:N+1]
-
-    cv[1:M+1, 1] = cv[1:M+1, N]
-     h[1:M+1, N] =  h[1:M+1, 1]
-    cu[2:M+1, N] = cu[2:M+1, 1]
-     z[2:M+1, 1] =  z[2:M+1, N]
-
-    cu[1, N]     = cu[M, 1]
-    cv[M, 1]     = cv[1, N]
-     z[1, 1]     =  z[M, N]
-     h[M, N]     =  h[1, 1]
-
-    tdts8 = tdt / 8.0::Float64
-    tdtsdx = tdt / dx
-    tdtsdy = tdt / dy
-
-    #unew[2:end,1:end-1]= uold[2:end,1:end-1] .+ tdts8 .* 
-    #                        (z[2:end, 2:end] .+ z[2:end, 1:end-1]) .*
-    #                        (cv[2:end,2:end] .+ cv[2:end,1:end-1] .+ 
-    #                        cv[1:end-1,2:end] .+ cv[1:end-1,1:end-1]) .-
-    #                        tdtsdx .* (h[2:end, 1:end-1] .- h[1:end-1,1:end-1])
-    calc_unew!(uold,z,cv,h,unew,tdts8,tdtsdx)
-
-    # vnew[1:end-1,2:end] = vold[1:end-1,2:end] .- tdts8 .*
-    #                         (z[2:end, 2:end] .+ z[1:end-1, 2:end]) .*
-    #                        (cu[2:end,2:end] .+ cu[2:end,1:end-1] .+ 
-    #                        cu[1:end-1,2:end] .+ cu[1:end-1,1:end-1]) .-
-    #                        tdtsdy .* (h[1:end-1, 2:end] .- h[1:end-1,1:end-1])
-    calc_vnew!(vold,z,cu,h,vnew,tdts8,tdtsdy)
-
-    # pnew[1:end-1,1:end-1] = pold[1:end-1,1:end-1] .- tdtsdx .*
-    #                         (cu[2:end, 1:end-1] .- cu[1:end-1,1:end-1]) .-
-    #                         tdtsdy .* (cv[1:end-1, 2:end] .- cv[1:end-1,1:end-1])
-    calc_pnew!(pold,cu,cv,pnew,tdtsdx)
+   # read_uvp(step::Int, suffix::String, M::Int, N::Int):
+   if ncycle == 1
+       # foo,bar,me = read_uvp(ncycle,"init",M,N)
+       validate_uvp(u,v,p,M,N,ncycle,"init")
+   end
+   # if VAL_DEEP and ncycle <= 3
+   #    validate_uvp(u,v,p,M,N,ncycle,'init')
+   calc_cu!(p,u,cu)
+   calc_cv!(p,v,cv)
 
 
-    # @time begin
-    unew[1, 1:end] = unew[M, 1:end]
-    pnew[M, 1:end] = pnew[1, 1:end]
-    vnew[M, 2:end] = vnew[1, 2:end]
 
-    unew[2:end, N] = unew[2:end, 1]
-    vnew[1:end, 1] = vnew[1:end, N]
-    pnew[1:end, N] = pnew[1:end, 1]
+   #  z = ((fsdx .* d_xi(v)) .- (fsdy .* d_yi(u)))./dp(p)
+   # z[2:end, 2:end] = (fsdx .* (v[2:end, 2:end] .- v[1:end-1, 2:end]) .- 
+   #                  fsdy .* (u[2:end, 2:end] .- u[2:end, 1:end-1])) ./
+   #                  (p[1:end-1,1:end-1] .+ p[2:end,1:end-1] .+ 
+   #                 p[2:end,2:end] .+ p[1:end-1,2:end])
+   calc_z!(v,u,p,z,fsdx,fsdy)
 
-    unew[1, N]     = unew[M, 1]
-    vnew[M, 1]     = vnew[1, N]
-    pnew[M, N]     = pnew[1, 1]
-    # end
+   calc_h!(p,u,v,h)
+    
+   cu[1,1:N+1]  = cu[M, 1:N+1]
+    h[M,1:N+1]  =  h[1, 1:N+1]
+   cv[M, 2:N+1] = cv[1, 2:N+1]
+    # z[M, 2:N+1] =  z[1, 2:N+1]
 
-    if ncycle>1
-        uold = u .+ alpha .* (unew .- 2.0::Float64 .* u .+ uold)
-        vold = v .+ alpha .* (vnew .- 2.0::Float64 .* v .+ vold)
-        pold = p .+ alpha .* (pnew .- 2.0::Float64 .* p .+ pold)
+   cv[1:M+1, 1] = cv[1:M+1, N]
+    h[1:M+1, N] =  h[1:M+1, 1]
+   cu[2:M+1, N] = cu[2:M+1, 1]
+    # z[2:M+1, 1] =  z[2:M+1, N]
 
-        u = unew
-        v = vnew
-        p = pnew
-    else
-        tdt = tdt + tdt
-        uold = u
-        vold = v
-        pold = p
-        u    = unew
-        v    = vnew
-        p    = pnew
-    end
+   cu[1, N]     = cu[M, 1]
+   cv[M, 1]     = cv[1, N]
+    # z[1, 1]     =  z[M, N]
+    h[M, N]     =  h[1, 1]
 
+   tdts8 = tdt / 8.0::Float64
+   tdtsdx = tdt / dx
+   tdtsdy = tdt / dy
+
+   # calc_unew!(uold,z,cv,h,unew,tdts8,tdtsdx)
+   # calc_vnew!(vold,z,cu,h,vnew,tdts8,tdtsdy)
+   # calc_pnew!(pold,cu,cv,pnew,tdtsdx)
+   calc_uvpnew!(uold,vold,pold,cu,cv,z,h,unew,vnew,pnew,tdts8,tdtsdy)
+
+
+   unew[1, 1:N+1] = unew[M, 1:N+1]
+   pnew[M, 1:N+1] = pnew[1, 1:N+1]
+   vnew[M, 2:N+1] = vnew[1, 2:N+1]
+
+   unew[2:M+1, N] = unew[2:M+1, 1]
+   vnew[1:M+1, 1] = vnew[1:M+1, N]
+   pnew[1:M+1, N] = pnew[1:M+1, 1]
+
+   unew[1, N]     = unew[M, 1]
+   vnew[M, 1]     = vnew[1, N]
+   pnew[M, N]     = pnew[1, 1]
+
+   if ncycle>1
+       uold = u .+ alpha .* (unew .- 2.0::Float64 .* u .+ uold)
+       vold = v .+ alpha .* (vnew .- 2.0::Float64 .* v .+ vold)
+       pold = p .+ alpha .* (pnew .- 2.0::Float64 .* p .+ pold)
+
+       u = unew
+       v = vnew
+       p = pnew
+   else
+       tdt = tdt + tdt
+       uold = u
+       vold = v
+       pold = p
+       u    = unew
+       v    = vnew
+       p    = pnew
+   end
+
+   if mod(ncycle,10) == 0
+      println(" NCYCLE is ",ncycle)
+      meminfo_julia()
+   end
 end
 end
 
