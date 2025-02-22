d7b38fa9f48e52b10641ad8039ec458cd4d7ffbc
Hilario Torres <hilario.c.torres@gmail.com>
2025-02-21 16:43:36 -0800

    Create mp4 movie of solution.

--- swm_AMReX/plotting_utils/convert_to_hdf5_and_plot.sh
+++ swm_AMReX/plotting_utils/convert_to_hdf5_and_plot.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
 
 # This script is used to convert all AMReX plotfiles in a specified directory to HDF5 format
-# and then plot the hdf5 files using matplotlib.
+# and then plot the hdf5 files using matplotlib. The script will also create an MP4 movie from the
+# PNG files created by the plotting script.
 
 ###############################################################################
 # User Input
 ###############################################################################
 plotfile2hdf5_exe="$SWM_AMREX_ROOT"/plotting_utils/main2d.gnu.MPI.ex
 
+###############################################################################
+# Command Line Input
+###############################################################################
 # Input directory argument must be provided
 if [ -z "$1" ]; then
     echo "Usage: $0 <directory with amrex plotfiles of the form plt_*>"
@@ -20,6 +24,8 @@ input_directory="$1"
 # Remove trailing slash from input_directory if it exists... so the pattern in the for loop below works
 input_directory="${input_directory%/}"
 
+###############################################################################
+# Setup
 ###############################################################################
 
 set -u
@@ -38,6 +44,10 @@ print_banner() {
     echo -e "$border\n"
 }
 
+
+###############################################################################
+# Convert to HDF5 and Plot to create PNG files
+###############################################################################
 
 # Loop over all files that start with plt_ followed by a series of numbers in the specified directory
 for infile in "$input_directory"/plt[0-9]*; do
@@ -60,4 +70,21 @@ for infile in "$input_directory"/plt[0-9]*; do
 
     python "$SWM_AMREX_ROOT"/plotting_utils/plot_hdf5.py "$hdf5_file" --output_dir "$input_directory"
 
+done
+
+##############################################################################
+# Create MP4 Movie from PNG Files
+##############################################################################
+print_banner "Creating MP4 Movies"
+
+# Ensure ffmpeg is available
+if ! command -v ffmpeg &> /dev/null
+then
+    echo "ffmpeg could not be found, please install it or add it to your path to create the MP4 movies."
+    exit 1
+fi
+
+# Create the MP4 movie from PNG files
+for prefix in u v p; do
+    ffmpeg -y -framerate 4 -pattern_type glob -i "${prefix}_*.png" -c:v libx264 -pix_fmt yuv420p "${prefix}.mp4"
 done
