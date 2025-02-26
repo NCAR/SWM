fc03b24b58a3ca03ec08bc9e734ce188cd55e5bf
Hilario Torres <hilario.c.torres@gmail.com>
2025-02-26 13:23:55 -0800

    Bug Fix. Sign error in when computing z in UpdateIntermediateVariablesKernel.

--- swm_AMReX/swm_mini_app_kernels.h
+++ swm_AMReX/swm_mini_app_kernels.h
@@ -16,7 +16,7 @@ void UpdateIntermediateVariablesKernel( const int i, const int j, const int k,
 {
     cu(i,j,k) = 0.5*(p(i,j,k) + p(i+1,j,k))*u(i,j,k);
     cv(i,j,k) = 0.5*(p(i,j,k) + p(i,j+1,k))*v(i,j,k);
-    z(i,j,k) = (fsdx*(v(i+1,j,k)-v(i,j,k)) + fsdy*(u(i,j+1,k)-u(i,j,k)))/(p(i,j,k)+p(i+1,j,k)+p(i,j+1,k)+p(i+1,j+1,k));
+    z(i,j,k) = (fsdx*(v(i+1,j,k)-v(i,j,k)) - fsdy*(u(i,j+1,k)-u(i,j,k)))/(p(i,j,k)+p(i+1,j,k)+p(i,j+1,k)+p(i+1,j+1,k));
     h(i,j,k) = p(i,j,k) + 0.25*(u(i-1,j,k)*u(i-1,j,k) + u(i,j,k)*u(i,j,k) + v(i,j-1,k)*v(i,j-1,k) + v(i,j,k)*v(i,j,k));
 }
 
