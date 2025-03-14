ee949d12f83b86a190403ad51b6ca759f8fecf9d
Hilario Torres <hilario.c.torres@gmail.com>
2025-03-14 15:37:21 -0700

    Update scaling plots to show more than one experiment at a time.

--- swm_AMReX/doc/plot_runtime_speedup_efficiency.py
+++ swm_AMReX/doc/plot_runtime_speedup_efficiency.py
@@ -17,22 +17,38 @@ figure_size=(8,8)
 #line_type='-*'
 
 # Deriechio MPI... 4096x4096, 1024 max chunk size, 100 time steps 
-n_procs = np.array([1, 2, 4, 8, 16])
-t = np.array([25.01, 16.52, 16.42, 16.39, 15.44])
-legend_label='Derecho MPI'
-line_type='-o'
+# No MPI optimizations
+derecho_MPI_old = {
+    'n_procs': np.array([1, 2, 4, 8, 16]),
+    't': np.array([25.01, 16.52, 16.42, 16.39, 15.44]),
+    'legend_label': 'Derecho MPI - Original',
+    'line_type': '-o'
+}
+
+# Deriechio MPI... 4096x4096, 1024 max chunk size, 100 time steps 
+# Using swap instead of copy. Made sure that partitioning and mapping look ok. Removed several uneeded fill boundary calls for multfabs.
+derecho_MPI_new = {
+    'n_procs': np.array([1, 2, 4, 8, 16]),
+    't': np.array([21.94, 13.92, 13.65, 13.83, 13.22]),
+    'legend_label': 'Derecho MPI - Swap + Less fill boundary',
+    'line_type': '-o'
+}
+
+cases_to_plot = [derecho_MPI_old, derecho_MPI_new]
 
 ###############################################################################
 # Runtime Plot
 ###############################################################################
 plt.figure(figsize=figure_size)
 
-plt.plot(n_procs, t, line_type, label=legend_label)
+for case in cases_to_plot:
+  plt.plot(case['n_procs'], case['t'], case['line_type'], label=case['legend_label'])
 
 plt.xlabel(r'$N_{\mathrm{proc}}$',fontsize=label_font_size)
 plt.ylabel(r'Runtime [s]',fontsize=label_font_size)
 plt.legend()
 plt.tight_layout()
+
 
 ###############################################################################
 # Speedup Plot
@@ -40,12 +56,14 @@ plt.tight_layout()
 plt.figure(figsize=figure_size)
 
 # Ideal speedup for reference
-plt.plot(n_procs, n_procs, '--k', label='Ideal')
+n_procs_max = max([np.max(case['n_procs']) for case in cases_to_plot])
+plt.plot([1,n_procs_max], [1,n_procs_max], '--k', label='Ideal')
 
 # Actual speedup
-assert(n_procs[0] == 1) # Serial time must be the first element or speedup calculation y label will be misleading
-t_serial = t[0]
-plt.plot(n_procs, t_serial/t, line_type, label=legend_label)
+for case in cases_to_plot:
+  assert(case['n_procs'][0] == 1) # Serial time must be the first element or speedup calculation y label will be misleading
+  t_serial = case['t'][0]
+  plt.plot(case['n_procs'], t_serial/case['t'], case['line_type'], label=case['legend_label'])
 
 plt.xlabel(r'$N_{\mathrm{proc}}$',fontsize=label_font_size)
 plt.ylabel(r'Speedup $= \frac{t_{\mathrm{serial}}}{t_\mathrm{parallel}}$',fontsize=label_font_size)
@@ -59,12 +77,13 @@ plt.tight_layout()
 plt.figure(figsize=figure_size)
 
 # Ideal efficiency for reference
-plt.plot(n_procs, 1.0*np.ones(len(n_procs)), '--k', label='Ideal')
+plt.plot([1,n_procs_max], [1,1], '--k', label='Ideal')
 
 # Actual efficiency
-assert(n_procs[0] == 1) # Serial time must be the first element or speedup calculation y label will be misleading
-t_serial = t[0]
-plt.plot(n_procs, t_serial/(n_procs*t), line_type, label=legend_label)
+for case in cases_to_plot:
+  assert(case['n_procs'][0] == 1) # Serial time must be the first element or speedup calculation y label will be misleading
+  t_serial = case['t'][0]
+  plt.plot(case['n_procs'], t_serial/(case['n_procs']*case['t']), case['line_type'], label=case['legend_label'])
 
 plt.xlabel(r'$N_{\mathrm{proc}}$',fontsize=label_font_size)
 plt.ylabel(r'Efficiency $= \frac{\mathrm{Speedup}}{N_{\mathrm{proc}}} =  \frac{t_{\mathrm{serial}}}{N_{\mathrm{proc}}t_{\mathrm{parallel}}}$',fontsize=label_font_size)
