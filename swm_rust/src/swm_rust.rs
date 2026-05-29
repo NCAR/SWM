1baf8009e18018d3d4934b8cffd9f750daec4f01
Riley Fisher <rileyfisher26@gmail.com>
2026-05-29 13:37:20 -0600

    initial commit of rust version, follows syntax in C version

--- swm_rust/src/swm_rust.rs
+++ swm_rust/src/swm_rust.rs
@@ -1,6 +1,8 @@
 // Timing and environment stuff 
-use std::time::Instant;
-use std::env;
+// use std::time::Instant;
+// use std::env;
+
+use std::f64::consts;
 
 // Utils is a helper module that contains some utility functions in src/utils.rs
 mod utils;
@@ -8,15 +10,191 @@ use utils::*;
 
 fn main() {
 
-    let args: Vec<String> = env::args().collect();
+    // -----------------------------------------------------------------------
+    // Simulation Parameters and Constants Setup
+    // -----------------------------------------------------------------------
 
-    let in_val: i32 = args[1].parse().expect("Input not an integer");
+    // parameters
+    let dt: f64 = 90.;
+    let mut tdt: f64 = dt;
 
-    let start = Instant::now();
-    println!("Hello, world!");
-    let result = rand_util(in_val);
-    println!("Result: {}", result);
+    let dx: f64 = 100000.;
+    let dy: f64 = 100000.;
+    let fsdx: f64 = 4. / dx;
+    let fsdy: f64 = 4. / dy;
 
-    let elapsed_time = start.elapsed();
-    println!("Elapsed time: {:?}", elapsed_time.as_secs_f64());
+    let a: f64 = 1000000.;
+    let alpha: f64 = 0.001;
+
+    let el: f64 = N as f64 * dx;
+    let pi = consts::PI;
+    let tpi: f64 = pi + pi;
+    let di: f64 = tpi / M as f64;
+    let dj: f64 = tpi / N as f64;
+    let pcf: f64 = pi * pi * a * a / (el * el);
+
+    // indices
+    let mut idx: usize;
+    let mut idx01: usize;
+    let mut idx10: usize;
+    let mut idx11: usize;
+
+    // -----------------------------------------------------------------------
+    // Define Solution Arrays
+    // -----------------------------------------------------------------------
+
+    let mut u: [f64; TOT_LEN] = [0.0; TOT_LEN];
+    let mut v: [f64; TOT_LEN] = [0.0; TOT_LEN];
+    let mut p: [f64; TOT_LEN] = [0.0; TOT_LEN];
+    let mut unew: [f64; TOT_LEN] = [0.0; TOT_LEN];
+    let mut vnew: [f64; TOT_LEN] = [0.0; TOT_LEN];
+    let mut pnew: [f64; TOT_LEN] = [0.0; TOT_LEN];
+    let mut uold: [f64; TOT_LEN] = [0.0; TOT_LEN];
+    let mut vold: [f64; TOT_LEN] = [0.0; TOT_LEN];
+    let mut pold: [f64; TOT_LEN] = [0.0; TOT_LEN];
+    let mut cu: [f64; TOT_LEN] = [0.0; TOT_LEN];
+    let mut cv: [f64; TOT_LEN] = [0.0; TOT_LEN];
+    let mut z: [f64; TOT_LEN] = [0.0; TOT_LEN];
+    let mut h: [f64; TOT_LEN] = [0.0; TOT_LEN];
+    let mut psi: [f64; TOT_LEN] = [0.0; TOT_LEN];
+
+    // -----------------------------------------------------------------------
+    // Initialize Data
+    // -----------------------------------------------------------------------
+
+    // initialize stream function psi and pressure p
+    for i in 0..M_LEN {
+        for j in 0..N_LEN {
+            idx = ij_to_idx(i,j); // [i][j]
+            psi[idx] = a * ( ( (i as f64) + 0.5 ) * di ).sin() * ( ( (j as f64) + 0.5 ) * dj ).sin();
+            p[idx] = pcf * ( ( 2.0 * (i as f64) * di ).cos() + ( 2.0 * (j as f64) * dj ).cos() ) + 50000.;
+        }
+    }
+
+    // initialize velocities u and v
+    for i in 0..M {
+        for j in 0..N {
+            idx01 = ij_to_idx(i,j+1); // [i][j+1]
+            idx10 = ij_to_idx(i+1,j); // [i+1][j]
+            idx11 = ij_to_idx(i+1,j+1); // [i+1][j+1]
+            u[idx10] = -(psi[idx11] - psi[idx10]) / dy;
+            v[idx01] = (psi[idx11] - psi[idx01]) / dx;
+        }
+    }
+
+    // periodic boundary conditions
+    apply_uv_bcs(&mut u, &mut v);
+
+    // initialize old arrays
+    for i in 0..M_LEN {
+        for j in 0..N_LEN {
+            idx = ij_to_idx(i,j);
+            uold[idx] = u[idx];
+            vold[idx] = v[idx];
+            pold[idx] = p[idx];
+        }
+    }
+
+    // print out initial values
+    // println!(" number of points in the x direction {:}", N); 
+    // println!(" number of points in the y direction {:}", M); 
+    // println!(" grid spacing in the x direction     {:}", dx); 
+    // println!(" grid spacing in the y direction     {:}", dy); 
+    // println!(" time step                           {:}", dt); 
+    // println!(" time filter parameter               {:}", alpha);
+
+    // let mnmin = M.min(N);
+    // println!(" initial diagonal elements of p");
+    // for i in 0..mnmin {
+    //   print!("{:.6} ",p[ij_to_idx(i,i)]);
+    // }
+    // println!("\n initial diagonal elements of u");
+    // for i in 0..mnmin {
+    //   print!("{:.6} ",u[ij_to_idx(i,i)]);
+    // }
+    // println!("\n initial diagonal elements of v");
+    // for i in 0..mnmin {
+    //   print!("{:.6} ",v[ij_to_idx(i,i)]);
+    // }
+    // print!("\n");
+
+    // print initial values to txt files
+    // print_data_to_file("uinit_rust.txt", &u);
+    // print_data_to_file("vinit_rust.txt", &v);
+    // print_data_to_file("pinit_rust.txt", &p);
+
+    // -----------------------------------------------------------------------
+    // Time Marching Loop
+    // -----------------------------------------------------------------------
+
+    for ncycle in 1..=ITMAX { // fix
+        // compute intermediate variables cu, cv, z, and h using u, v, and p
+        update_intermed_vars(&u, &v, &p, fsdx, fsdy, &mut cu, &mut cv, &mut z, &mut h);
+
+        // apply periodic boundary conditions to intermediate variables
+        apply_intermed_bcs(&mut cu, &mut cv, &mut z, &mut h);
+
+        // time update to new variables
+        let tdts8 = tdt / 8.0;
+        let tdtsdx = tdt / dx;
+        let tdtsdy = tdt / dy;
+        time_update_new_vars(&uold, &vold, &pold, &cu, &cv, &z, &h, tdts8, tdtsdx, tdtsdy, &mut unew, &mut vnew, &mut pnew);
+        
+        // apply periodic boundary conitions to new variables
+        apply_uvp_bcs(&mut unew, &mut vnew, &mut pnew);
+
+        // update update old vars and solution
+        if ncycle > 1 {
+            // smooth old vars using time filter
+            smooth_update_old_vars(&u, &v, &p, &unew, &vnew, &pnew, &mut uold, &mut vold, &mut pold, alpha);
+
+            // update u, v, and p to new solution
+            u.copy_from_slice(&unew);
+            v.copy_from_slice(&vnew);
+            p.copy_from_slice(&pnew);
+        } else {
+            // update tdt for subsequent timesteps
+            tdt = tdt + tdt;
+
+            // no smoothing for first timestep
+            // this might be redundant
+            uold.copy_from_slice(&u);
+            vold.copy_from_slice(&v);
+            pold.copy_from_slice(&p);
+
+            // update u, v, and p to new solution
+            // might be able to take out of if statement
+            u.copy_from_slice(&unew);
+            v.copy_from_slice(&vnew);
+            p.copy_from_slice(&pnew);
+        }
+    }
+
+    // -----------------------------------------------------------------------
+    // End
+    // -----------------------------------------------------------------------
+    
+    // print data to txt files
+    // print_data_to_file("cu_rust.txt", &cu);
+    // print_data_to_file("cv_rust.txt", &cv);
+    // print_data_to_file("z_rust.txt", &z);
+    // print_data_to_file("h_rust.txt", &h);
+    // print_data_to_file("u_rust.txt", &u);
+    // print_data_to_file("v_rust.txt", &v);
+    // print_data_to_file("p_rust.txt", &p);
+
+
+    // code for arg parsing and timing
+
+    // let args: Vec<String> = env::args().collect();
+
+    // let in_val: i32 = args[1].parse().expect("Input not an integer");
+
+    // let start = Instant::now();
+    // println!("Hello, world!");
+    // let result = rand_util(in_val);
+    // println!("Result: {}", result);
+
+    // let elapsed_time = start.elapsed();
+    // println!("Elapsed time: {:?}", elapsed_time.as_secs_f64());
 }
--- swm_rust/src/utils.rs
+++ swm_rust/src/utils.rs
@@ -1,10 +1,155 @@
+use std::fs::File;
+use std::path::Path;
+use std::io::prelude::*;
+
+// declare constants
+pub const M: usize = 256;
+pub const N: usize = 256;
+pub const M_LEN: usize = M+1;
+pub const N_LEN: usize = N+1;
+pub const TOT_LEN: usize = (M_LEN)*(N_LEN);
+pub const ITMAX: usize = 1000;
 
 // Below is a dummy variable to test stuff. 
-pub fn rand_util(in_val: i32) -> f64 {
-    let mut x = 0.0;
-    for i in 0..in_val {
-        x += (i as f64).sin() * (i as f64).cos();
+// pub fn rand_util(in_val: i32) -> f64 {
+//     let mut x = 0.0;
+//     for i in 0..in_val {
+//         x += (i as f64).sin() * (i as f64).cos();
+//     }
+//     // Default we return the last line that has no semicolon, but we can also use the return keyword if needed.
+//     x
+// }
+
+pub fn ij_to_idx(i: usize, j: usize) -> usize {
+    i*N_LEN + j
+}
+
+pub fn apply_uv_bcs(u: &mut [f64], v: &mut [f64]) {
+    for j in 0..N {
+        u[ij_to_idx(0,j)] = u[ij_to_idx(M,j)];
+        v[ij_to_idx(M,j+1)] = v[ij_to_idx(0,j+1)];
     }
-    // Default we return the last line that has no semicolon, but we can also use the return keyword if needed.
-    x
+
+    for i in 0..M {
+        u[ij_to_idx(i+1,N)] = u[ij_to_idx(i+1,0)];
+        v[ij_to_idx(i,0)] = v[ij_to_idx(i,N)];
+    }
+
+    u[ij_to_idx(0,N)] = u[ij_to_idx(M,0)];
+    v[ij_to_idx(M,0)] = v[ij_to_idx(0,N)];
+}
+
+pub fn update_intermed_vars(u: &[f64], v: &[f64], p: &[f64], fsdx: f64, fsdy: f64, cu: &mut [f64], cv: &mut [f64], z: &mut [f64], h: &mut [f64]) {
+    let mut idx00: usize;
+    let mut idx01: usize;
+    let mut idx10: usize;
+    let mut idx11: usize;
+    for i in 0..M {
+        for j in 0..N {
+            idx00 = ij_to_idx(i,j);
+            idx01 = ij_to_idx(i,j+1);
+            idx10 = ij_to_idx(i+1,j);
+            idx11 = ij_to_idx(i+1,j+1);
+            cu[idx10] = 0.5 * (p[idx10] + p[idx00]) * u[idx10];
+            cv[idx01] = 0.5 * (p[idx01] + p[idx00]) * v[idx01];
+            z[idx11] = (fsdx * (v[idx11] - v[idx01]) - fsdy * (u[idx11] - u[idx10])) / (p[idx00] + p[idx10] + p[idx11] + p[idx01]);
+            h[idx00] = p[idx00] + 0.25 * (u[idx10] * u[idx10] + u[idx00] * u[idx00] + v[idx01] * v[idx01] + v[idx00] * v[idx00]);
+        }
+    }
+}
+
+pub fn apply_intermed_bcs(cu: &mut [f64], cv: &mut [f64], z: &mut [f64], h: &mut [f64]) {
+    for j in 0..N {
+        cu[ij_to_idx(0,j)] = cu[ij_to_idx(M,j)];
+        cv[ij_to_idx(M,j+1)] = cv[ij_to_idx(0,j+1)];
+        z[ij_to_idx(0,j+1)] = z[ij_to_idx(M,j+1)];
+        h[ij_to_idx(M,j)] = h[ij_to_idx(0,j)];
+    }
+    
+    for i in 0..M {
+        cu[ij_to_idx(i+1,N)] = cu[ij_to_idx(i+1,0)];
+        cv[ij_to_idx(i,0)] = cv[ij_to_idx(i,N)];
+        z[ij_to_idx(i+1,0)] = z[ij_to_idx(i+1,N)];
+        h[ij_to_idx(i,N)] = h[ij_to_idx(i,0)];
+    }
+
+    cu[N] = cu[M*N_LEN];
+    cv[M*N_LEN] = cv[N];
+    z[0] = z[M*N_LEN+N];
+    h[M*N_LEN+N] = h[0];
+}
+
+pub fn time_update_new_vars(uold: &[f64], vold: &[f64], pold: &[f64], cu: &[f64], cv: &[f64], z: &[f64], h: &[f64], tdts8: f64, tdtsdx: f64, tdtsdy: f64, unew: &mut [f64], vnew: &mut [f64], pnew: &mut [f64]) {
+    let mut idx00: usize;
+    let mut idx01: usize;
+    let mut idx10: usize;
+    let mut idx11: usize;
+    for i in 0..M {
+        for j in 0..N {
+            idx00 = ij_to_idx(i,j);
+            idx01 = ij_to_idx(i,j+1);
+            idx10 = ij_to_idx(i+1,j);
+            idx11 = ij_to_idx(i+1,j+1);
+            unew[idx10] = uold[idx10] + tdts8 * (z[idx11] + z[idx10]) * (cv[idx11] + cv[idx01] + cv[idx00] + cv[idx10]) - tdtsdx * (h[idx10] - h[idx00]);
+            vnew[idx01] = vold[idx01] - tdts8 * (z[idx11] + z[idx01]) * (cu[idx11] + cu[idx01] + cu[idx00] + cu[idx10]) - tdtsdy * (h[idx01] - h[idx00]);
+            pnew[idx00] = pold[idx00] - tdtsdx * (cu[idx10] - cu[idx00]) - tdtsdy * (cv[idx01] - cv[idx00]);
+        }
+    }
+}
+
+pub fn apply_uvp_bcs(u: &mut [f64], v: &mut [f64], p: &mut [f64]) {
+    for j in 0..N {
+        u[ij_to_idx(0,j)] = u[ij_to_idx(M,j)];
+        v[ij_to_idx(M,j+1)] = v[ij_to_idx(0,j+1)];
+        p[ij_to_idx(M,j)] = p[ij_to_idx(0,j)];
+    }
+
+    for i in 0..M {
+        u[ij_to_idx(i+1,N)] = u[ij_to_idx(i+1,0)];
+        v[ij_to_idx(i,0)] = v[ij_to_idx(i,N)];
+        p[ij_to_idx(i,N)] = p[ij_to_idx(i,0)];
+    }
+
+    u[ij_to_idx(0,N)] = u[ij_to_idx(M,0)];
+    v[ij_to_idx(M,0)] = v[ij_to_idx(0,N)];
+    p[ij_to_idx(M,N)] = p[ij_to_idx(0,0)];
+}
+
+pub fn smooth_update_old_vars(u: &[f64], v: &[f64], p: &[f64], unew: &[f64], vnew: &[f64], pnew: &[f64], uold: &mut [f64], vold: &mut [f64], pold: &mut [f64], alpha: f64) {
+    let mut idx: usize;
+    for i in 0..M_LEN {
+        for j in 0..N_LEN {
+            idx = ij_to_idx(i,j);
+            uold[idx] = u[idx] + alpha * (unew[idx] - 2. * u[idx] + uold[idx]);
+            vold[idx] = v[idx] + alpha * (vnew[idx] - 2. * v[idx] + vold[idx]);
+            pold[idx] = p[idx] + alpha * (pnew[idx] - 2. * p[idx] + pold[idx]);
+        }
+    }
+}
+
+pub fn print_data_to_file(pathname: &str, data: &[f64]) {
+    // define path and display
+    let path = Path::new(pathname);
+    let display = path.display();
+
+    // Open a file in write-only mode, returns `io::Result<File>`
+    let mut file = match File::create(&path) {
+        Err(why) => panic!("couldn't create {}: {}", display, why),
+        Ok(file) => file,
+    };
+
+    // Create string from data
+    let mut s = String::from("");
+    for i in 0..M_LEN {
+        for j in 0..N_LEN {
+            s += &format!("{:.6} ", data[ij_to_idx(i,j)]);
+        }
+        s += "\n";
+    }
+
+    // Write string to `file`, returns `io::Result<()>`
+    match file.write_all(s.as_bytes()) {
+        Err(why) => panic!("couldn't write to {}: {}", display, why),
+        Ok(_) => println!("successfully wrote to {}", display),
+    }
 }
