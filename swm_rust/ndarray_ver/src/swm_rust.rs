703aa86bef66d90e53d39863a8493cfc6b400449
Riley Fisher <rileyfisher26@gmail.com>
2026-06-15 15:13:53 -0600

    exchange nested for loops for Zip

--- swm_rust/ndarray_ver/src/swm_rust.rs
+++ swm_rust/ndarray_ver/src/swm_rust.rs
@@ -2,9 +2,8 @@
 use std::time::Instant;
 // use std::env;
 
-use std::f64::consts;
 use std::mem;
-use ndarray::Array2;
+use ndarray::{Array2};
 
 // Utils is a helper module that contains some utility functions in src/utils.rs
 mod utils;
@@ -28,13 +27,6 @@ fn main() {
     let a: f64 = 1000000.;
     let alpha: f64 = 0.001;
 
-    let el: f64 = N as f64 * dx;
-    let pi = consts::PI;
-    let tpi: f64 = pi + pi;
-    let di: f64 = tpi / M as f64;
-    let dj: f64 = tpi / N as f64;
-    let pcf: f64 = pi * pi * a * a / (el * el);
-
     // -----------------------------------------------------------------------
     // Define Solution Arrays
     // -----------------------------------------------------------------------
@@ -52,72 +44,58 @@ fn main() {
     let mut cv = Array2::<f64>::zeros((M_LEN,N_LEN));
     let mut z = Array2::<f64>::zeros((M_LEN,N_LEN));
     let mut h = Array2::<f64>::zeros((M_LEN,N_LEN));
-    let mut psi = Array2::<f64>::zeros((M_LEN,N_LEN));
+
+    // intermediate arrays using with Zip
+    let mut term1 = Array2::<f64>::zeros((M,N));
+    let mut term2 = Array2::<f64>::zeros((M,N));
 
     // -----------------------------------------------------------------------
     // Initialize Data
     // -----------------------------------------------------------------------
 
-    // initialize stream function psi and pressure p
-    for i in 0..M_LEN {
-        for j in 0..N_LEN {
-            // let idx: usize = ij_to_idx(i,j); // [i][j]
-            psi[[i,j]] = a * ( ( (i as f64) + 0.5 ) * di ).sin() * ( ( (j as f64) + 0.5 ) * dj ).sin();
-            p[[i,j]] = pcf * ( ( 2.0 * (i as f64) * di ).cos() + ( 2.0 * (j as f64) * dj ).cos() ) + 50000.;
-        }
-    }
-
-    // initialize velocities u and v
-    for i in 0..M {
-        for j in 0..N {
-            // let idx01: usize = ij_to_idx(i,j+1); // [i][j+1]
-            // let idx10: usize = ij_to_idx(i+1,j); // [i+1][j]
-            // let idx11: usize = ij_to_idx(i+1,j+1); // [i+1][j+1]
-            u[[i+1,j]] = -(psi[[i+1,j+1]] - psi[[i+1,j]]) / dy;
-            v[[i,j+1]] = (psi[[i+1,j+1]] - psi[[i,j+1]]) / dx;
-        }
-    }
+    // initialize velocities u and v, pressure p
+    init_conds(&mut u, &mut v, &mut p, dx, dy, a);
 
     // periodic boundary conditions
     apply_uv_bcs(&mut u, &mut v);
 
     // initialize old arrays
-    for i in 0..M_LEN {
-        for j in 0..N_LEN {
-            // let idx: usize = ij_to_idx(i,j);
-            uold[[i,j]] = u[[i,j]];
-            vold[[i,j]] = v[[i,j]];
-            pold[[i,j]] = p[[i,j]];
-        }
-    }
+    // for i in 0..M_LEN {
+    //     for j in 0..N_LEN {
+    //         uold[[i,j]] = u[[i,j]];
+    //         vold[[i,j]] = v[[i,j]];
+    //         pold[[i,j]] = p[[i,j]];
+    //     }
+    // }
+
+    uold.assign(&u);
+    vold.assign(&v);
+    pold.assign(&p);
 
     // print out initial values
-    // println!(" number of points in the x direction {:}", N); 
-    // println!(" number of points in the y direction {:}", M); 
-    // println!(" grid spacing in the x direction     {:}", dx); 
-    // println!(" grid spacing in the y direction     {:}", dy); 
-    // println!(" time step                           {:}", dt); 
-    // println!(" time filter parameter               {:}", alpha);
+    if VERBOSE {
+        println!(" number of points in the x direction {:}", N); 
+        println!(" number of points in the y direction {:}", M); 
+        println!(" grid spacing in the x direction     {:}", dx); 
+        println!(" grid spacing in the y direction     {:}", dy); 
+        println!(" time step                           {:}", dt); 
+        println!(" time filter parameter               {:}", alpha);
 
-    // let mnmin = M.min(N);
-    // println!(" initial diagonal elements of p");
-    // for i in 0..mnmin {
-    //   print!("{:.6} ",p[ij_to_idx(i,i)]);
-    // }
-    // println!("\n initial diagonal elements of u");
-    // for i in 0..mnmin {
-    //   print!("{:.6} ",u[ij_to_idx(i,i)]);
-    // }
-    // println!("\n initial diagonal elements of v");
-    // for i in 0..mnmin {
-    //   print!("{:.6} ",v[ij_to_idx(i,i)]);
-    // }
-    // print!("\n");
-
-    // print initial values to txt files
-    // print_data_to_file("uinit_rust.txt", &u);
-    // print_data_to_file("vinit_rust.txt", &v);
-    // print_data_to_file("pinit_rust.txt", &p);
+        let mnmin = M.min(N);
+        println!(" initial diagonal elements of p");
+        for i in 0..mnmin {
+          print!("{:.6} ",p[[i,i]]);
+        }
+        println!("\n initial diagonal elements of u");
+        for i in 0..mnmin {
+          print!("{:.6} ",u[[i,i]]);
+        }
+        println!("\n initial diagonal elements of v");
+        for i in 0..mnmin {
+          print!("{:.6} ",v[[i,i]]);
+        }
+        print!("\n");
+    }
 
     // Start timer
     let tstart = Instant::now(); 
@@ -135,7 +113,7 @@ fn main() {
         let mut c1 = tstart.elapsed().as_secs_f64();
 
         // compute intermediate variables cu, cv, z, and h using u, v, and p
-        update_intermed_vars(&u, &v, &p, fsdx, fsdy, &mut cu, &mut cv, &mut z, &mut h);
+        update_intermed_vars(&u, &v, &p, fsdx, fsdy, &mut cu, &mut cv, &mut z, &mut h, &mut term1, &mut term2);
 
         let mut c2 = tstart.elapsed().as_secs_f64();
         t100 = t100 + (c2 - c1);
@@ -150,7 +128,7 @@ fn main() {
 
         c1 = tstart.elapsed().as_secs_f64();
 
-        time_update_new_vars(&uold, &vold, &pold, &cu, &cv, &z, &h, tdts8, tdtsdx, tdtsdy, &mut unew, &mut vnew, &mut pnew);
+        time_update_new_vars(&uold, &vold, &pold, &cu, &cv, &z, &h, tdts8, tdtsdx, tdtsdy, &mut unew, &mut vnew, &mut pnew, &mut term1, &mut term2);
         
         c2 = tstart.elapsed().as_secs_f64();
         t200 = t200 + (c2 - c1);
@@ -199,61 +177,51 @@ fn main() {
 
     let ptime = time / 3600.;
 
-    println!(" cycle number {:?} model time in hours {:?}\n", ITMAX, ptime);
-    // printf(" diagonal elements of p\n");
-    // for (i=0; i<mnmin; i++) {
-    //   printf("%f ",pnew[i*N_LEN+i]);
-    // }
-    // printf("\n diagonal elements of u\n");
-    // for (i=0; i<mnmin; i++) {
-    //   printf("%f ",unew[i*N_LEN+i]);
-    // }
-    // printf("\n diagonal elements of v\n");
-    // for (i=0; i<mnmin; i++) {
-    //   printf("%f ",vnew[i*N_LEN+i]);
-    // }
-    // printf("\n");
+    // print out final values
+    if VERBOSE {
+        let mnmin = M.min(N);
+        println!(" diagonal elements of p");
+        for i in 0..mnmin {
+        print!("{:?} ",pnew[[i,i]]);
+        }
+        println!("\n diagonal elements of u");
+        for i in 0..mnmin {
+        print!("{:?} ",unew[[i,i]]);
+        }
+        println!("\n diagonal elements of v");
+        for i in 0..mnmin {
+        print!("{:?} ",vnew[[i,i]]);
+        }
+        print!("\n");
+    }
+    // print out timings
+    if TIMING {
+        println!(" cycle number {:?} model time in hours {:?}\n", ITMAX, ptime);
 
-    let mut mfs100 = 0.0;
-    let mut mfs200 = 0.0;
-    let mut mfs300 = 0.0;
-    // gdr t100 etc. now an accumulation of all l100 time
-    if t100 > 0. { mfs100 = ITMAX as f64 * 24. * M as f64 * N as f64 / t100 / 1000000.; }
-    if t200 > 0. { mfs200 = ITMAX as f64 * 26. * M as f64 * N as f64 / t200 / 1000000.; }
-    if t300 > 0. { mfs300 = ITMAX as f64 * 15. * M as f64 * N as f64 / t300 / 1000000.; }
+        let mut mfs100 = 0.0;
+        let mut mfs200 = 0.0;
+        let mut mfs300 = 0.0;
+        // gdr t100 etc. now an accumulation of all l100 time
+        if t100 > 0. { mfs100 = ITMAX as f64 * 24. * M as f64 * N as f64 / t100 / 1000000.; }
+        if t200 > 0. { mfs200 = ITMAX as f64 * 26. * M as f64 * N as f64 / t200 / 1000000.; }
+        if t300 > 0. { mfs300 = ITMAX as f64 * 15. * M as f64 * N as f64 / t300 / 1000000.; }
 
-    let tcyc = ctime / ITMAX as f64;
+        let tcyc = ctime / ITMAX as f64;
 
-    println!(" cycle number {:?} total computer time {:?} time per cycle {:?}", ITMAX, ctime, tcyc);
-    println!(" time and megaflops for loop 100 {:?} {:?}", t100, mfs100);
-    println!(" time and megaflops for loop 200 {:?} {:?}", t200, mfs200);
-    println!(" time and megaflops for loop 300 {:?} {:?}", t300, mfs300);
+        println!(" cycle number {:?} total computer time {:?} time per cycle {:?}", ITMAX, ctime, tcyc);
+        println!(" time and megaflops for loop 100 {:?} {:?}", t100, mfs100);
+        println!(" time and megaflops for loop 200 {:?} {:?}", t200, mfs200);
+        println!(" time and megaflops for loop 300 {:?} {:?}", t300, mfs300);
+    }
+    // save solutions to txt files
+    if VAL_OUT {
+        print_data_to_file("u_rust.txt", &u);
+        print_data_to_file("v_rust.txt", &v);
+        print_data_to_file("p_rust.txt", &p);
+    }
 
     // -----------------------------------------------------------------------
     // End
     // -----------------------------------------------------------------------
-    
-    // print data to txt files
-    // print_data_to_file("cu_rust.txt", &cu);
-    // print_data_to_file("cv_rust.txt", &cv);
-    // print_data_to_file("z_rust.txt", &z);
-    // print_data_to_file("h_rust.txt", &h);
-    // print_data_to_file("u_rust.txt", &u);
-    // print_data_to_file("v_rust.txt", &v);
-    // print_data_to_file("p_rust.txt", &p);
 
-
-    // code for arg parsing and timing
-
-    // let args: Vec<String> = env::args().collect();
-
-    // let in_val: i32 = args[1].parse().expect("Input not an integer");
-
-    // let start = Instant::now();
-    // println!("Hello, world!");
-    // let result = rand_util(in_val);
-    // println!("Result: {}", result);
-
-    // let elapsed_time = start.elapsed();
-    // println!("Elapsed time: {:?}", elapsed_time.as_secs_f64());
 }
--- swm_rust/ndarray_ver/src/utils.rs
+++ swm_rust/ndarray_ver/src/utils.rs
@@ -1,29 +1,72 @@
 use std::fs::File;
 use std::path::Path;
 use std::io::prelude::*;
-use ndarray::Array2;
+use std::f64::consts;
+use ndarray::{Array2,Zip,s};
 
 // declare constants
 pub const M: usize = 256;
 pub const N: usize = 256;
 pub const M_LEN: usize = M+1;
 pub const N_LEN: usize = N+1;
-pub const TOT_LEN: usize = (M_LEN)*(N_LEN);
-pub const ITMAX: usize = 1000;
+pub const ITMAX: usize = 4000;
+pub const VERBOSE: bool = false;  // print out initial and final values
+pub const TIMING: bool = true;    // print out timings
+pub const VAL_OUT: bool = false;  // save final solutions to txt files
 
-// Below is a dummy variable to test stuff. 
-// pub fn rand_util(in_val: i32) -> f64 {
-//     let mut x = 0.0;
-//     for i in 0..in_val {
-//         x += (i as f64).sin() * (i as f64).cos();
-//     }
-//     // Default we return the last line that has no semicolon, but we can also use the return keyword if needed.
-//     x
-// }
+pub fn init_conds(u: &mut Array2<f64>, v: &mut Array2<f64>, p: &mut Array2<f64>, dx: f64, dy: f64, a: f64) { 
+    // init psi
+    let mut psi = Array2::<f64>::zeros((M_LEN,N_LEN));
 
-// pub fn ij_to_idx(i: usize, j: usize) -> usize {
-//     i*N_LEN + j
-// }
+    // set params
+    let el: f64 = N as f64 * dx;
+    let pi = consts::PI;
+    let tpi: f64 = pi + pi;
+    let di: f64 = tpi / M as f64;
+    let dj: f64 = tpi / N as f64;
+    let pcf: f64 = pi * pi * a * a / (el * el);
+    
+    // initialize stream function psi and pressure p
+    // for i in 0..M_LEN {
+    //     for j in 0..N_LEN {
+    //         psi[[i,j]] = a * ( ( (i as f64) + 0.5 ) * di ).sin() * ( ( (j as f64) + 0.5 ) * dj ).sin();
+    //         p[[i,j]] = pcf * ( ( 2.0 * (i as f64) * di ).cos() + ( 2.0 * (j as f64) * dj ).cos() ) + 50000.;
+    //     }
+    // }
+
+    Zip::indexed(psi.view_mut())
+        .for_each(|(i, j), val| {
+            *val = a * ( ( (i as f64) + 0.5 ) * di ).sin() * ( ( (j as f64) + 0.5 ) * dj ).sin();
+        });
+    Zip::indexed(p.view_mut())
+        .for_each(|(i, j), val| {
+            *val = pcf * ( ( 2.0 * (i as f64) * di ).cos() + ( 2.0 * (j as f64) * dj ).cos() ) + 50000.;
+        });
+
+    // initialize velocities u and v
+    // for i in 0..M {
+    //     for j in 0..N {
+    //         // let idx01: usize = ij_to_idx(i,j+1); // [i][j+1]
+    //         // let idx10: usize = ij_to_idx(i+1,j); // [i+1][j]
+    //         // let idx11: usize = ij_to_idx(i+1,j+1); // [i+1][j+1]
+    //         u[[i+1,j]] = -(psi[[i+1,j+1]] - psi[[i+1,j]]) / dy;
+    //         v[[i,j+1]] = (psi[[i+1,j+1]] - psi[[i,j+1]]) / dx;
+    //     }
+    // }
+    
+    Zip::from(u.slice_mut(s![1.., ..N]))
+        .and(psi.slice(s![1.., 1..]))
+        .and(psi.slice(s![1.., ..N]))
+        .for_each(|u_val, &psi_up, &psi_down| {
+            *u_val = -(psi_up - psi_down) / dy;
+        });
+    Zip::from(v.slice_mut(s![..M, 1..]))
+        .and(psi.slice(s![1.., 1..]))
+        .and(psi.slice(s![..M, 1..]))
+        .for_each(|v_val, &psi_right, &psi_left| {
+            *v_val = (psi_right - psi_left) / dx;
+        });
+}
 
 pub fn apply_uv_bcs(u: &mut Array2<f64>, v: &mut Array2<f64>) {
     for j in 0..N {
@@ -40,19 +83,61 @@ pub fn apply_uv_bcs(u: &mut Array2<f64>, v: &mut Array2<f64>) {
     v[[M,0]] = v[[0,N]];
 }
 
-pub fn update_intermed_vars(u: &Array2<f64>, v: &Array2<f64>, p: &Array2<f64>, fsdx: f64, fsdy: f64, cu: &mut Array2<f64>, cv: &mut Array2<f64>, z: &mut Array2<f64>, h: &mut Array2<f64>) {
-    for i in 0..M {
-        for j in 0..N {
-            // let idx00 = ij_to_idx(i,j);
-            // let idx01 = ij_to_idx(i,j+1);
-            // let idx10 = ij_to_idx(i+1,j);
-            // let idx11 = ij_to_idx(i+1,j+1);
-            cu[[i+1,j]] = 0.5 * (p[[i+1,j]] + p[[i,j]]) * u[[i+1,j]];
-            cv[[i,j+1]] = 0.5 * (p[[i,j+1]] + p[[i,j]]) * v[[i,j+1]];
-            z[[i+1,j+1]] = (fsdx * (v[[i+1,j+1]] - v[[i,j+1]]) - fsdy * (u[[i+1,j+1]] - u[[i+1,j]])) / (p[[i,j]] + p[[i+1,j]] + p[[i+1,j+1]] + p[[i,j+1]]);
-            h[[i,j]] = p[[i,j]] + 0.25 * (u[[i+1,j]] * u[[i+1,j]] + u[[i,j]] * u[[i,j]] + v[[i,j+1]] * v[[i,j+1]] + v[[i,j]] * v[[i,j]]);
-        }
-    }
+pub fn update_intermed_vars(u: &Array2<f64>, v: &Array2<f64>, p: &Array2<f64>, fsdx: f64, fsdy: f64, cu: &mut Array2<f64>, cv: &mut Array2<f64>, z: &mut Array2<f64>, h: &mut Array2<f64>, term1: &mut Array2<f64>, term2: &mut Array2<f64>) {
+    // for i in 0..M {
+    //     for j in 0..N {
+            // cu[[i+1,j]] = 0.5 * (p[[i+1,j]] + p[[i,j]]) * u[[i+1,j]];
+            // cv[[i,j+1]] = 0.5 * (p[[i,j+1]] + p[[i,j]]) * v[[i,j+1]];
+            // z[[i+1,j+1]] = (fsdx * (v[[i+1,j+1]] - v[[i,j+1]]) - fsdy * (u[[i+1,j+1]] - u[[i+1,j]])) / (p[[i,j]] + p[[i+1,j]] + p[[i+1,j+1]] + p[[i,j+1]]);
+            // h[[i,j]] = p[[i,j]] + 0.25 * (u[[i+1,j]] * u[[i+1,j]] + u[[i,j]] * u[[i,j]] + v[[i,j+1]] * v[[i,j+1]] + v[[i,j]] * v[[i,j]]);
+    //     }
+    // }
+
+    // cu
+    Zip::from(cu.slice_mut(s![1.., ..N]))
+        .and(p.slice(s![1.., ..N]))
+        .and(p.slice(s![..M, ..N]))
+        .and(u.slice(s![1.., ..N]))
+        .for_each(|cu10, &p10, &p00, &u10| {
+            *cu10 = 0.5 * (p10 + p00) * u10;
+        });
+    
+    // cv
+    Zip::from(cv.slice_mut(s![..M, 1..]))
+        .and(p.slice(s![..M, 1..]))
+        .and(p.slice(s![..M, ..N]))
+        .and(v.slice(s![..M, 1..]))
+        .for_each(|cv01, &p01, &p00, &v01| {
+            *cv01 = 0.5 * (p01 + p00) * v01;
+        });
+    
+    // z
+    Zip::from(term1.view_mut())
+        .and(v.slice(s![1.., 1..]))
+        .and(v.slice(s![..M, 1..]))
+        .and(u.slice(s![1.., 1..]))
+        .and(u.slice(s![1.., ..N]))
+        .for_each(|val, &v11, &v01, &u11, &u10| {
+            *val = fsdx * (v11 - v01) - fsdy * (u11 - u10);
+        });
+    four_node_sum(p, term2);
+    Zip::from(z.slice_mut(s![1.., 1..]))
+        .and(term1.view())
+        .and(term2.view())
+        .for_each(|z11, &val1, &val2| {
+            *z11 = val1 / val2;
+        });
+    
+    // h
+    Zip::from(h.slice_mut(s![..M, ..N]))
+        .and(u.slice(s![1.., ..N]))
+        .and(u.slice(s![..M, ..N]))
+        .and(v.slice(s![..M, 1..]))
+        .and(v.slice(s![..M, ..N]))
+        .and(p.slice(s![..M, ..N]))
+        .for_each(|h00, &u10, &u00, &v01, &v00, &p00| {
+            *h00 = &p00 + 0.25 * (&u10 * &u10 + &u00 * &u00 + &v01 * &v01 + &v00 * &v00);
+        });
 }
 
 pub fn apply_intermed_bcs(cu: &mut Array2<f64>, cv: &mut Array2<f64>, z: &mut Array2<f64>, h: &mut Array2<f64>) {
@@ -70,28 +155,67 @@ pub fn apply_intermed_bcs(cu: &mut Array2<f64>, cv: &mut Array2<f64>, z: &mut Ar
         h[[i,N]] = h[[i,0]];
     }
 
-    // cu[N] = cu[M*N_LEN];
-    // cv[M*N_LEN] = cv[N];
-    // z[0] = z[M*N_LEN+N];
-    // h[M*N_LEN+N] = h[0];
     cu[[0,N]] = cu[[M,0]];
     cv[[M,0]] = cv[[0,N]];
     z[[0,0]] = z[[M,N]];
     h[[M,N]] = h[[0,0]];
 }
 
-pub fn time_update_new_vars(uold: &Array2<f64>, vold: &Array2<f64>, pold: &Array2<f64>, cu: &Array2<f64>, cv: &Array2<f64>, z: &Array2<f64>, h: &Array2<f64>, tdts8: f64, tdtsdx: f64, tdtsdy: f64, unew: &mut Array2<f64>, vnew: &mut Array2<f64>, pnew: &mut Array2<f64>) {
-    for i in 0..M {
-        for j in 0..N {
-            // let idx00 = ij_to_idx(i,j);
-            // let idx01 = ij_to_idx(i,j+1);
-            // let idx10 = ij_to_idx(i+1,j);
-            // let idx11 = ij_to_idx(i+1,j+1);
-            unew[[i+1,j]] = uold[[i+1,j]] + tdts8 * (z[[i+1,j+1]] + z[[i+1,j]]) * (cv[[i+1,j+1]] + cv[[i,j+1]] + cv[[i,j]] + cv[[i+1,j]]) - tdtsdx * (h[[i+1,j]] - h[[i,j]]);
-            vnew[[i,j+1]] = vold[[i,j+1]] - tdts8 * (z[[i+1,j+1]] + z[[i,j+1]]) * (cu[[i+1,j+1]] + cu[[i,j+1]] + cu[[i,j]] + cu[[i+1,j]]) - tdtsdy * (h[[i,j+1]] - h[[i,j]]);
-            pnew[[i,j]] = pold[[i,j]] - tdtsdx * (cu[[i+1,j]] - cu[[i,j]]) - tdtsdy * (cv[[i,j+1]] - cv[[i,j]]);
-        }
-    }
+pub fn time_update_new_vars(uold: &Array2<f64>, vold: &Array2<f64>, pold: &Array2<f64>, cu: &Array2<f64>, cv: &Array2<f64>, z: &Array2<f64>, h: &Array2<f64>, tdts8: f64, tdtsdx: f64, tdtsdy: f64, unew: &mut Array2<f64>, vnew: &mut Array2<f64>, pnew: &mut Array2<f64>, term1: &mut Array2<f64>, term2: &mut Array2<f64>) {
+    // for i in 0..M {
+    //     for j in 0..N {
+            // unew[[i+1,j]] = uold[[i+1,j]] + tdts8 * (z[[i+1,j+1]] + z[[i+1,j]]) * (cv[[i+1,j+1]] + cv[[i,j+1]] + cv[[i,j]] + cv[[i+1,j]]) - tdtsdx * (h[[i+1,j]] - h[[i,j]]);
+            // vnew[[i,j+1]] = vold[[i,j+1]] - tdts8 * (z[[i+1,j+1]] + z[[i,j+1]]) * (cu[[i+1,j+1]] + cu[[i,j+1]] + cu[[i,j]] + cu[[i+1,j]]) - tdtsdy * (h[[i,j+1]] - h[[i,j]]);
+            // pnew[[i,j]] = pold[[i,j]] - tdtsdx * (cu[[i+1,j]] - cu[[i,j]]) - tdtsdy * (cv[[i,j+1]] - cv[[i,j]]);
+    //     }
+    // }
+
+    // unew
+    four_node_sum(cv, term1);
+    Zip::from(term2.view_mut())
+        .and(z.slice(s![1.., 1..]))
+        .and(z.slice(s![1.., ..N]))
+        .and(term1.view())
+        .for_each(|val2, &z11, &z10, &val1| {
+            *val2 = tdts8 * (z11 + z10) * val1;
+        });
+    Zip::from(unew.slice_mut(s![1.., ..N]))
+        .and(term2.view())
+        .and(uold.slice(s![1.., ..N]))
+        .and(h.slice(s![1.., ..N]))
+        .and(h.slice(s![..M, ..N]))
+        .for_each(|unew10, &val2, &uold10, &h10, &h00| {
+            *unew10 = uold10 + val2 - tdtsdx * (h10 - h00);
+        });
+
+    // vnew
+    four_node_sum(cu, term1);
+    Zip::from(term2.view_mut())
+        .and(z.slice(s![1.., 1..]))
+        .and(z.slice(s![..M, 1..]))
+        .and(term1.view())
+        .for_each(|val2, &z11, &z01, &val1| {
+            *val2 = - tdts8 * (z11 + z01) * val1;
+        });
+    Zip::from(vnew.slice_mut(s![..M, 1..]))
+        .and(term2.view())
+        .and(vold.slice(s![0..M, 1..]))
+        .and(h.slice(s![..M, 1..]))
+        .and(h.slice(s![..M, ..N]))
+        .for_each(|vnew01, &val2, &vold01, &h01, &h00| {
+            *vnew01 = vold01 + val2 - tdtsdy * (h01 - h00);
+        });
+
+    // pnew
+    Zip::from(pnew.slice_mut(s![..M, ..N]))
+        .and(pold.slice(s![..M, ..N]))
+        .and(cu.slice(s![1.., ..N]))
+        .and(cu.slice(s![..M, ..N]))
+        .and(cv.slice(s![..M, 1..]))
+        .and(cv.slice(s![..M, ..N]))
+        .for_each(|pnew00, &pold00, &cu10, &cu00, &cv01, &cv00| {
+            *pnew00 = pold00 - tdtsdx * (cu10 - cu00) - tdtsdy * (cv01 - cv00);
+        });
 }
 
 pub fn apply_uvp_bcs(u: &mut Array2<f64>, v: &mut Array2<f64>, p: &mut Array2<f64>) {
@@ -113,14 +237,48 @@ pub fn apply_uvp_bcs(u: &mut Array2<f64>, v: &mut Array2<f64>, p: &mut Array2<f6
 }
 
 pub fn smooth_update_old_vars(u: &Array2<f64>, v: &Array2<f64>, p: &Array2<f64>, unew: &Array2<f64>, vnew: &Array2<f64>, pnew: &Array2<f64>, uold: &mut Array2<f64>, vold: &mut Array2<f64>, pold: &mut Array2<f64>, alpha: f64) {
-    for i in 0..M_LEN {
-        for j in 0..N_LEN {
-            // let idx = ij_to_idx(i,j);
-            uold[[i,j]] = u[[i,j]] + alpha * (unew[[i,j]] - 2. * u[[i,j]] + uold[[i,j]]);
-            vold[[i,j]] = v[[i,j]] + alpha * (vnew[[i,j]] - 2. * v[[i,j]] + vold[[i,j]]);
-            pold[[i,j]] = p[[i,j]] + alpha * (pnew[[i,j]] - 2. * p[[i,j]] + pold[[i,j]]);
-        }
-    }
+    // for i in 0..M_LEN {
+    //     for j in 0..N_LEN {
+    //         uold[[i,j]] = u[[i,j]] + alpha * (unew[[i,j]] - 2. * u[[i,j]] + uold[[i,j]]);
+    //         vold[[i,j]] = v[[i,j]] + alpha * (vnew[[i,j]] - 2. * v[[i,j]] + vold[[i,j]]);
+    //         pold[[i,j]] = p[[i,j]] + alpha * (pnew[[i,j]] - 2. * p[[i,j]] + pold[[i,j]]);
+    //     }
+    // }
+
+    // uold
+    Zip::from(uold.view_mut())
+        .and(u)
+        .and(unew)
+        .for_each(|uold00, &u00, &unew00| {
+            *uold00 = u00 + alpha * (unew00 - 2. * u00 + *uold00);
+        });
+
+    // vold
+    Zip::from(vold.view_mut())
+        .and(v)
+        .and(vnew)
+        .for_each(|vold00, &v00, &vnew00| {
+            *vold00 = v00 + alpha * (vnew00 - 2. * v00 + *vold00);
+        });
+
+    // pold
+    Zip::from(pold.view_mut())
+        .and(p)
+        .and(pnew)
+        .for_each(|pold00, &p00, &pnew00| {
+            *pold00 = p00 + alpha * (pnew00 - 2. * p00 + *pold00);
+        });
+}
+
+pub fn four_node_sum(arr: &Array2<f64>, sum: &mut Array2<f64>) {
+    Zip::from(sum.view_mut())
+        .and(arr.slice(s![1.., 1..]))
+        .and(arr.slice(s![..M, 1..]))
+        .and(arr.slice(s![..M, ..N]))
+        .and(arr.slice(s![1.., ..N]))
+        .for_each(|val, &arr11, &arr01, &arr00, &arr10| {
+            *val = arr11 + arr01 + arr00 + arr10;
+        });
 }
 
 pub fn print_data_to_file(pathname: &str, data: &Array2<f64>) {
