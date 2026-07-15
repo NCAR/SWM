a25cdcd50b51148a71a9dec88ee352c9c69d1001
Riley Fisher <rileyfisher26@gmail.com>
2026-07-15 11:43:10 -0600

    remove bindgen

--- swm_rust/gpu-cudarc/build.rs
+++ swm_rust/gpu-cudarc/build.rs
@@ -3,7 +3,6 @@ extern crate cc;
 
 use std::{env, path::PathBuf, process::Command};
 
-use bindgen::CargoCallbacks;
 use regex::Regex;
 
 fn main() {
@@ -34,24 +33,6 @@ fn main() {
         nvcc_status.success(),
         "Failed to compile CUDA source to PTX."
     );
-
-    // The bindgen::Builder is the main entry point
-    // to bindgen, and lets you build up options for
-    // the resulting bindings.
-    let bindings = bindgen::Builder::default()
-        // The input header we would like to generate
-        // bindings for.
-        .header("src/cuda/includes/wrapper.h")
-        // Tell cargo to invalidate the built crate whenever any of the
-        // included header files changed.
-        .parse_callbacks(Box::new(CargoCallbacks))
-        // we use "no_copy" and "no_debug" here because we don't know if we can safely generate them for our structs in C code (they may contain raw pointers)
-        .no_copy("*")
-        .no_debug("*")
-        // Finish the builder and generate the bindings.
-        .generate()
-        // Unwrap the Result and panic on failure.
-        .expect("Unable to generate bindings");
 
     // we need to make modifications to the generated code
     let generated_bindings = bindings.to_string();
